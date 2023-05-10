be600a094b5230c7458db89c04457e0b927d7ff2
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-10 18:38:12 +0200

    a

--- resources/views/configurations/vehicle/brands.blade.php
+++ resources/views/configurations/vehicle/brands.blade.php
@@ -91,40 +91,33 @@
                             </td>
 
                             <td class="text-start">
-                                <a href="#" class="btn btn-light btn-active-light-primary btn-sm"
-                                   data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                    Actions
-                                    <span class="svg-icon svg-icon-5 m-0"><svg width="24" height="24"
-                                                                               viewBox="0 0 24 24"
-                                                                               fill="none"
-                                                                               xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z"
-                                            fill="currentColor"></path>
-                                    </svg>
-                                </span>
-
-                                    <div
-                                        class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
-                                        data-kt-menu="true">
-
-                                        <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">
+                                <div class="dropdown">
+                                    <button class="btn btn-light btn-active-light-primary btn-sm dropdown-toggle"
+                                            type="button"
+                                            id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                                            aria-expanded="false">
+                                        Actions
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li>
+                                            <a class="dropdown-item"
+                                               data-zfm-action="edit"
+                                               href="#">
                                                 Edit
                                             </a>
-                                        </div>
+                                        </li>
 
-                                        <div class="menu-item px-3">
-                                            <a href="#" data-kt-action="remove" data-kt-table-filter="delete_row"
-                                               class="menu-link px-3">
+                                        <li>
+                                            <a class="dropdown-item"
+                                               href="#"
+                                               data-zfm-action="remove"
+                                               data-kt-table-filter="delete_row">
                                                 Delete
                                             </a>
-                                        </div>
-
-                                    </div>
-                                </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </td>
-
                         </tr>
                         </tbody>
                     </table>
