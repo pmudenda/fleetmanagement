134aa2fa46ad58109cef2d0138bb6adf0e31fe57
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-11 06:10:33 +0200

    change first to get on requisition details

--- app/Services/Requisitions/FuelRequisitionService.php
+++ app/Services/Requisitions/FuelRequisitionService.php
@@ -243,7 +243,7 @@ class FuelRequisitionService
             ->join('GEN_MATERIAL_DETAILS', 'GEN_MATERIAL_HEADERS.req_no', '=', 'GEN_MATERIAL_DETAILS.req_no')
             ->leftJoin('CONFIG_STATUSES', 'GEN_MATERIAL_HEADERS.status', '=', 'CONFIG_STATUSES.code')
             ->select('GEN_MATERIAL_HEADERS.*', 'GEN_MATERIAL_DETAILS.*', 'CONFIG_STATUSES.name as status_name', 'CONFIG_STATUSES.color_code')
-            ->first();
+            ->get();
 
         return $results->first();
 
