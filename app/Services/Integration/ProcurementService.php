794c5df1af835ddd5fcda2c081e024dd412aed45
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-13 17:42:16 +0200

    a

--- app/Services/Integration/ProcurementService.php
+++ app/Services/Integration/ProcurementService.php
@@ -13,11 +13,12 @@ class ProcurementService
 
         $requisitionType = empty($requisitionType) ? 'seq_store_req' : '';
 
-        Log::info(' Generating Document For '. $requisitionType .' and Area ' .$area);
-        $result = DB::scalar("select storesDocumentNumberGenerator($requisitionType, $area) as value from dual");
-        //$result = DB::executeFunction('storesDocumentNumberGenerator', ['ls_type' => $requisitionType, 'ls_area'=> $area], PDO::PARAM_STR);
-        Log::info('Document Number ' . $result);
-        return $result;//->value;
+        Log::info(' Generating Document For ' . $requisitionType . ' and Area ' . $area);
+        //$result = DB::scalar("select storesDocumentNumberGenerator($requisitionType, $area) as value from dual");
+        //$result =
+        DB::executeFunction('storesDocumentNumberGenerator', ['ls_type' => $requisitionType, 'ls_area' => $area], PDO::PARAM_STR);
+        //Log::info('Document Number ' . $result);
+        return "";// $result->value;
     }
 
 }
