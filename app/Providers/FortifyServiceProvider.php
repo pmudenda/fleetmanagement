a750221769db150c20da89bcf10b27b7355a8756
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-13 11:15:34 +0200

    update namespaces

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,6 +6,7 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Helpers\ConfigHelper;
 use App\Helpers\StatusHelper;
 use App\Models\Security\User;
 use Carbon\Carbon;
