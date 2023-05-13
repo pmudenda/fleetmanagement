0f5fd1bda9e95576b994151e766a7ac87e7a62aa
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-13 18:00:08 +0200

    a

--- app/Services/Integration/ProcurementService.php
+++ app/Services/Integration/ProcurementService.php
@@ -8,11 +8,11 @@ use PDO;
 
 class ProcurementService
 {
-    public function generateDocumentNumber($requisitionType, $area): string
+    public function generateDocumentNumber(string $requisitionType, string $area): string
     {
         Log::info(' Generating Document For ' . $requisitionType . ' and Area ' . $area);
         //$result = DB::scalar("select storesDocumentNumberGenerator($requisitionType, $area) as value from dual");
-        $result = DB::executeFunction('storesDocumentNumberGenerator', [':ls_type' => $requisitionType, ':ls_area' => $area], PDO::PARAM_STR);
+        $result = DB::executeFunction('storesDocumentNumberGenerator', ['ls_type' => trim($requisitionType), 'ls_area' => trim($area)], PDO::PARAM_STR);
         //Log::info('Document Number ' . $result);
         return "";// $result->value;
     }
