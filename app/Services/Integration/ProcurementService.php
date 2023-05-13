39fbe55517a6394348ac9c385494bb152b261eb3
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-13 18:37:56 +0200

    a

--- app/Services/Integration/ProcurementService.php
+++ app/Services/Integration/ProcurementService.php
@@ -13,19 +13,20 @@ class ProcurementService
             Log::info(' Generating Document For ' . $doc_type . ' and Area ' . $area_code);
             //$result = DB::selectOne("select storesDocumentNumberGenerator($doc_type, $area_code) as value from dual");
 
-            $pdo = DB::getPdo();
+           /* $pdo = DB::getPdo();
             $stmt = $pdo->prepare("begin :result := storesDocumentNumberGenerator(:ls_type, :ls_area); end;");
-            oci_bind_by_name($stmt,':result',$result);
+            oci_bind_by_name($stmt,':result',$ls_return);
             //$stmt->bindParam(':result', $result);
             $stmt->bindParam(':ls_type', $doc_type);
             $stmt->bindParam(':ls_area', $area_code);
-            $stmt->execute();
+            $stmt->execute();*/
+            $results = DB::select('select storesDocumentNumberGenerator(:ls_type, :ls_area)', ['ls_type' => $doc_type,'ls_area'=>$area_code ]);
             /*$result = DB::executeFunction(
                 'storesDocumentNumberGenerator',
                 ['ls_type' => trim($requisitionType), 'ls_area' => trim($area)],
                 PDO::PARAM_STR);*/
-            Log::info('Document Number ' . $result);
-            return $result; //->value;
+            Log::info('Document Number ');
+            return ""; //->value;
         } catch (\Exception $e) {
             Log::error($e);
             return "";
