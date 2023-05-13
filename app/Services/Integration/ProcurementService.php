0728d0458ac3aeb19b75115f47fff5958babe210
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-13 18:43:44 +0200

    a

--- app/Services/Integration/ProcurementService.php
+++ app/Services/Integration/ProcurementService.php
@@ -13,19 +13,19 @@ class ProcurementService
             Log::info(' Generating Document For ' . $doc_type . ' and Area ' . $area_code);
             //$result = DB::selectOne("select storesDocumentNumberGenerator($doc_type, $area_code) as value from dual");
 
-           /* $pdo = DB::getPdo();
-            $stmt = $pdo->prepare("begin :result := storesDocumentNumberGenerator(:ls_type, :ls_area); end;");
-            oci_bind_by_name($stmt,':result',$ls_return);
-            //$stmt->bindParam(':result', $result);
-            $stmt->bindParam(':ls_type', $doc_type);
-            $stmt->bindParam(':ls_area', $area_code);
-            $stmt->execute();*/
-            $results = DB::select('select storesDocumentNumberGenerator(:ls_type, :ls_area) as value from dual', ['ls_type' => $doc_type,'ls_area'=>$area_code ]);
+            /* $pdo = DB::getPdo();
+             $stmt = $pdo->prepare("begin :result := storesDocumentNumberGenerator(:ls_type, :ls_area); end;");
+             oci_bind_by_name($stmt,':result',$ls_return);
+             //$stmt->bindParam(':result', $result);
+             $stmt->bindParam(':ls_type', $doc_type);
+             $stmt->bindParam(':ls_area', $area_code);
+             $stmt->execute();*/
+            $results = DB::select('select storesDocumentNumberGenerator(:ls_type, :ls_area) as value from dual', ['ls_type' => $doc_type, 'ls_area' => $area_code]);
             /*$result = DB::executeFunction(
                 'storesDocumentNumberGenerator',
                 ['ls_type' => trim($requisitionType), 'ls_area' => trim($area)],
                 PDO::PARAM_STR);*/
-            Log::info('Document Number '. $results->value);
+            Log::info('Document Number ' . implode(',', $results));
             return ""; //->value;
         } catch (\Exception $e) {
             Log::error($e);
