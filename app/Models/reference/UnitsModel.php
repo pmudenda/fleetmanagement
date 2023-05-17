d2a071f53a8f54c826522bea22453622f4b4c9e9
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-17 08:16:09 +0200

    projects

--- app/Models/reference/UnitsModel.php
+++ app/Models/reference/UnitsModel.php
@@ -5,8 +5,8 @@ namespace App\Models\reference;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BatteryModel extends Model
+class UnitsModel extends Model
 {
     use HasFactory;
-    protected $table = 'BATTERY_VIEW';
+    protected $table = 'UNITS_VIEW';
 }
