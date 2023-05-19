376b46465d00604b344dfb8e41891bf304a50b6f
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-19 13:21:22 +0200

    a

--- resources/views/vehicleManagement/details/view.blade.php
+++ resources/views/vehicleManagement/details/view.blade.php
@@ -165,7 +165,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Vehicle Disk</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">File Viewer</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
