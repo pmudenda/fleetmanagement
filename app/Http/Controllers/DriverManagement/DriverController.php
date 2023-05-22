459702a7dc5c4d315f973656bb738e0101d5bfda
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-22 12:18:05 +0200

    license classes creation

--- app/Http/Controllers/DriverManagement/DriverController.php
+++ app/Http/Controllers/DriverManagement/DriverController.php
@@ -2,36 +2,19 @@
 
 namespace App\Http\Controllers\DriverManagement;
 
-use App\Constants\ErrorMessages;
 use App\Enums\ConfigurationTypes;
-use App\Helpers\StatusHelper;
 use App\Http\Controllers\Controller;
 use App\Models\configurations\GeneralTableConfigurations;
-use App\Models\general\BusinessUnits;
-use App\Models\general\CostCenters;
-use App\Models\reference\PHCMSEmployee;
-use App\Models\Security\Role;
-use App\Models\Security\User;
-use App\Services\Security\ParameterEncryption;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
 
 class DriverController extends Controller
 {
 
     public function index(): View|\Illuminate\Foundation\Application|Factory|Application
     {
-        $licenseClasses = GeneralTableConfigurations::where('type', ConfigurationTypes::LICENSE_CLASS.value())->get();
+        $licenseClasses = GeneralTableConfigurations::where('type', ConfigurationTypes::LICENSE_CLASS->value)->get();
         return view('modules.driverManagement.index')->with(compact('licenseClasses'));
     }
 
@@ -41,7 +24,8 @@ class DriverController extends Controller
         return view('modules.driverManagement.driverList')->with(compact('users'));
     }
 
-    public function findDriver(){
+    public function findDriver()
+    {
 
     }
 }
