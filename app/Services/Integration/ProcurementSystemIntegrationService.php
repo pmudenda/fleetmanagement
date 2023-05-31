515b755849d1ce195a733a986a7fad4c8505fbb0
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-05-31 10:58:14 +0200

    change way to execute function

--- app/Services/Integration/ProcurementSystemIntegrationService.php
+++ app/Services/Integration/ProcurementSystemIntegrationService.php
@@ -44,19 +44,20 @@ class ProcurementSystemIntegrationService
 
             $pdo = DB::getPdo();
 
+            $user = auth()->user()->staff_no;
             $stmt = $pdo->prepare("begin :result := fn_create_stores_req(:p_ref_no, :p_reg_no, :p_store_code, :p_user_requesting, :p_job_card, :p_system_origin, :p_fleet_req_code, :p_req_acc_number, :p_delivery_site, :p_transaction_type, :p_current_user); end;");
             $stmt->bindParam(':result', $results);
             $stmt->bindParam(':p_ref_no', $doc_no);
             $stmt->bindParam(':p_reg_no', $veh_reg_no);
             $stmt->bindParam(':p_store_code', $stores_code);
-            $stmt->bindParam(':p_user_requesting', auth()->user()->staff_no);
+            $stmt->bindParam(':p_user_requesting', $user);
             $stmt->bindParam(':p_job_card', $job_card_no);
             $stmt->bindParam(':p_system_origin', $ZESCOFleetMaster);
             $stmt->bindParam(':p_fleet_req_code', $stores_requisition_number);
             $stmt->bindParam(':p_req_acc_number', $account);
             $stmt->bindParam(':p_delivery_site', $delivery_site);
             $stmt->bindParam(':p_transaction_type', $transactionType);
-            $stmt->bindParam(':p_current_user', auth()->user()->staff_no);
+            $stmt->bindParam(':p_current_user', $user);
             $stmt->execute();
 
             /*$results = DB::select(
