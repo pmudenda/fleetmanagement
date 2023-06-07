d71e7649d6e1157f372da35b8c5cd1af69aa2658
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-06-07 22:18:15 +0200

    a

--- resources/views/modules/requisitions/maintenance/tabs/partsSelection.blade.php
+++ resources/views/modules/requisitions/maintenance/tabs/partsSelection.blade.php
@@ -99,19 +99,19 @@
                     <div class="container-fluid pl-0">
                         <div class="row">
                             <div class="form-group row">
-                                <label
+                                {{--<label
                                     class="col-xs-12 col-sm-6 col-md-7 col-lg-4"
                                     for="job_card_no">
-                                    Time In:
-                                </label>
+
+                                </label>--}}
                                 <div class="col-xs-12 col-sm-6 col-md-7 col-lg-7">
-                                    <input type="text"
+                                    {{--<input type="text"
                                            readonly
                                            value="@if($details){{Carbon::parse($details->time_in)->format('H:i:s')}}@else{{Carbon::now()->format('H:i:s')}}@endif"
                                            class="form-control form-control-sm when_valid number_input"
                                            id="timeIn"
                                            name="timeIn"
-                                    />
+                                    />--}}
                                 </div>
                             </div>
                         </div>
@@ -188,28 +188,6 @@
                                         />
                                     @endif
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div id="accidentRecordNo" class="row d-none">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                    <div class="container-fluid pl-0">
-                        <div class="row">
-                            <div class="form-group row">
-                                <label
-                                    class="col-xs-12 col-sm-6 col-md-5 col-lg-4 field-required"
-                                    for="staff_name">
-                                    Accident No:
-                                </label>
-                                <div class="col-xs-12 col-sm-6 col-md-7 col-lg-7">
-                                    <select name="accident_number" id="accident_number"
-                                            class="form-control form-select-sm when_valid"
-                                            required>
-                                    </select>
                                 </div>
                             </div>
                         </div>
