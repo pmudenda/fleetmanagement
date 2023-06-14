a5d979ddf16b71e96cc96707f17f641c70de2789
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-06-14 19:14:00 +0200

    update background image

--- resources/views/modules/requisitions/maintenance/show.blade.php
+++ resources/views/modules/requisitions/maintenance/show.blade.php
@@ -305,7 +305,7 @@
                                     </div>
 
                                     <div class="row">
-                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="col-xs-12 col-sm-6 col-md-6">
                                             <div class="container-fluid pl-0">
                                                 <div class="row">
                                                     <div class="form-group row">
