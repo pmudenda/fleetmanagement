d911df7ff7337ba68b09fcbdf619402cb93e421c
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-06-17 11:43:55 +0200

    added using

--- resources/views/modules/requisitions/maintenance/create.blade.php
+++ resources/views/modules/requisitions/maintenance/create.blade.php
@@ -282,9 +282,9 @@
                 const row = $(e.currentTarget).closest('tr');
 
                 $(row).find('[name="serviceArticleCode"]').val(article['id']);
-                $(row).find('[name="unit_price"]').val(article['price_map']);
+                $(row).find('[name="service_unit_price"]').val(article['price_map']);
                 $(row).find('[name="service_technical_specification"]').val(article['technical_specifications']);
-                $(row).find('[name="unit_of_measure"]').val(article['unit_measure_name']);
+                $(row).find('[name="service_unit_of_measure"]').val(article['unit_measure_name']);
             });
         }
 
