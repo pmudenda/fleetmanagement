70c301971feecbaf242981ea6cda7b2d80e54e7c
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-06-17 12:38:34 +0200

    added using

--- app/Enums/WorkflowProcessCodes.php
+++ app/Enums/WorkflowProcessCodes.php
@@ -8,5 +8,5 @@ enum WorkflowProcessCodes: string
     case OutOfTownFuelRequisition = "2001";
     case OverrideFuelRequisition = "2002";
     case StoresRequisition = "3000";
-    const PurchaseProcess = "3000";
+    case PurchaseProcess = "4000";
 }
