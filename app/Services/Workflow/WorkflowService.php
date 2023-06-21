c90b2da3bb1af3be917b67d68cbea96d50e54ed4
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-06-21 14:27:08 +0200

    a

--- app/Services/Workflow/WorkflowService.php
+++ app/Services/Workflow/WorkflowService.php
@@ -119,7 +119,7 @@ class WorkflowService
             'created_by' => $currentUser->id,
             'date_acted' => Carbon::now(),
             'process_code' => $processCode,
-            'amount' => $amount
+            'amount' => number_format($amount)
         ]);
 
         return WorkflowTaskDetail::create([
