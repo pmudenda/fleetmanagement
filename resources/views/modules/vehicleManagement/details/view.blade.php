2e632ad446cbf5e80e7aa174935987e5c0d26fd6
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-19 15:23:16 +0200

    added missing tab on details view

--- resources/views/modules/vehicleManagement/details/view.blade.php
+++ resources/views/modules/vehicleManagement/details/view.blade.php
@@ -56,6 +56,17 @@
                                 </a>
                             </li>
 
+                            <li class="nav-item" role="presentation" data-tab="tms_engine_details_tab">
+                                <a @if($step < 4 ) disabled="disabled"
+                                   @endif class="nav-link text-active-primary pb-5 @if($step == 4) active @endif"
+                                   data-bs-toggle="tab"
+                                   href="#tms_access_checkin_tab" aria-selected="false" role="tab"
+                                   tabindex="-1">
+                                    @include('layouts.partials.engine_icon')
+                                    Step 3: Accessories Check-in
+                                </a>
+                            </li>
+
                             <li class="nav-item" role="presentation" data-tab="tms_costing_valuation_tab">
                                 <a class="nav-link text-active-primary pb-5"
                                    data-bs-toggle="tab"
