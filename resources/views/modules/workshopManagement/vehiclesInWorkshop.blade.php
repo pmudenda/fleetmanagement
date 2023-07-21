b9d0ad41f553cf7b5f67e19bfbba6038aeff939d
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-21 16:27:43 +0200

    correcting workshopManagement references

--- resources/views/modules/workshopManagement/vehiclesInWorkshop.blade.php
+++ resources/views/modules/workshopManagement/vehiclesInWorkshop.blade.php
@@ -146,23 +146,27 @@
 
     <!-- The Modal -->
     <div class="modal" id="finderModal">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
 
                 <!-- Modal Header -->
                 <div class="modal-header">
-                    <h4 class="modal-title">Modal Heading</h4>
+                    <h4 class="modal-title">
+                        <div class="alert alert-warning" id="query"></div>
+                    </h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
                 <!-- Modal body -->
                 <div class="modal-body">
-                    <table id="filterProperty" class="table table-striped">
+                    <table id="filterProperty" class="table">
                         <tbody>
                         <tr>
                             <td>
                                 <select class="form-select" name="property">
-                                    <option value="workshopSection" >Section</option>
+                                    <option value="" disabled>--Select--</option>
+                                    <option value="userUnit">User Unit</option>
+                                    <option value="workshopSection">Section</option>
                                     <option value="workshop" >Workshop</option>
                                     <option value="dateIn" >Date In</option>
                                     <option value="dateOut" >Date Out</option>
@@ -177,7 +181,7 @@
                                 </select>
                             </td>
                             <td>
-                                <select class="form-select" name="value">
+                                <select class="form-select" name="filterValue">
                                 </select>
                             </td>
                         </tr>
