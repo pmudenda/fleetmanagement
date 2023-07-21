8b39df0f76a732d5306f916d02516ea7293f2bc2
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-21 17:36:14 +0200

    correcting workshopManagement references

--- app/Http/Requests/WorkshopExitRequest.php
+++ app/Http/Requests/WorkshopExitRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class WorkshopExitRequest extends FormRequest
@@ -11,13 +12,13 @@ class WorkshopExitRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
