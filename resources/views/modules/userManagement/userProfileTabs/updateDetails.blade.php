d441362db5e1d5d6e74c586423b2f8464c3fc9fc
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-24 18:25:08 +0200

    added edit option

--- resources/views/modules/userManagement/userProfileTabs/updateDetails.blade.php
+++ resources/views/modules/userManagement/userProfileTabs/updateDetails.blade.php
@@ -134,9 +134,9 @@
     </div>
 
     <div class="form-group row">
-        <label class="col-xs-12 col-sm-6 col-md-5 col-lg-3"
+        <label class="col-sm-2 col-form-label"
                for="mobile_no">Supervisor:</label>
-        <div class="col-xs-12 col-sm-6 col-md-7 col-lg-6">
+        <div class="col-sm-10">
             @canany([config('rights.user_update')])
                 <div class="input-group">
                     <input type="text"
@@ -176,11 +176,11 @@
     </div>
 
     <div class="form-group row">
-        <label class="col-xs-12 col-sm-6 col-md-5 col-lg-3 field-required"
+        <label class="col-sm-2 field-required col-form-label"
                for="user_profile">Profile: </label>
-        <div class="col-xs-12 col-sm-6 col-md-7 col-lg-6">
+        <div class="col-sm-10">
             <select name="user_profile" id="user_profile"
-                    class="form-control form-select-sm"
+                    class="form-select form-select-sm"
                     required>
                 <option value>--Choose Profile--</option>
                 @foreach ($roles as $groupName)
