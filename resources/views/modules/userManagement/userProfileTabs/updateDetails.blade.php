3490a85b1b8c014ee60695f7a364bf05665c9534
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-24 18:21:52 +0200

    added edit option

--- resources/views/modules/userManagement/userProfileTabs/updateDetails.blade.php
+++ resources/views/modules/userManagement/userProfileTabs/updateDetails.blade.php
@@ -176,15 +176,16 @@
     </div>
 
     <div class="form-group row">
+        {{dd(auth()->user()->roles()->first()->id)}}
         <label class="col-xs-12 col-sm-6 col-md-5 col-lg-3 field-required"
-               for="user_profile">Profile:</label>
+               for="user_profile">Profile: </label>
         <div class="col-xs-12 col-sm-6 col-md-7 col-lg-6">
             <select name="user_profile" id="user_profile"
                     class="form-control form-select-sm"
                     required>
                 <option value>--Choose Profile--</option>
                 @foreach ($roles as $groupName)
-                    @if($groupName == auth()->user()->roles()->first()->id)
+                    @if($groupName == 'default')
                         <option selected
                                 value="{{$groupName->id}}">{{$groupName->description}}</option>
                     @else
