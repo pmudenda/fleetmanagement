af775ab282408f599d24252502680b636e8ec98f
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-07-24 17:22:01 +0200

    added edit option

--- resources/views/modules/workshopManagement/jobCard/tabs/partsSelection.blade.php
+++ resources/views/modules/workshopManagement/jobCard/tabs/partsSelection.blade.php
@@ -6,23 +6,29 @@
            value="{{route('suppliers.list')}}"/>
     <ul class="nav nav-tabs" role="tablist">
         <li class="nav-item" style="list-style: none; width: 178px;">
-            <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">Spares</a>
+            <a class="nav-link active" data-toggle="tab" href="#spares" role="tab">Spares</a>
         </li>
         <li class="nav-item" style="list-style: none; width: 178px;">
-            <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab">Services</a>
+            <a class="nav-link" data-toggle="tab" href="#services" role="tab">Services</a>
+        </li>
+        <li class="nav-item" style="list-style: none; width: 178px;">
+            <a class="nav-link" data-toggle="tab" href="#imprest" role="tab">Imprest Buys</a>
         </li>
         <li class="nav-item" style="list-style: none; width: 178px; display: none;">
-            <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab">Labour</a>
+            <a class="nav-link" data-toggle="tab" href="#labour" role="tab">Labour</a>
         </li>
     </ul><!-- Tab panes -->
     <div class="tab-content">
-        <div class="tab-pane active" id="tabs-1" role="tabpanel">
+        <div class="tab-pane active" id="spares" role="tabpanel">
             @include('modules.workshopManagement.jobCard.tabs.materials')
         </div>
-        <div class="tab-pane" id="tabs-2" role="tabpanel">
+        <div class="tab-pane" id="services" role="tabpanel">
             @include('modules.workshopManagement.jobCard.tabs.services')
         </div>
-        <div class="tab-pane" id="tabs-3" role="tabpanel">
+        <div class="tab-pane" id="imprest" role="tabpanel">
+            @include('modules.workshopManagement.jobCard.tabs.imprest_buy')
+        </div>
+        <div class="tab-pane" id="labour" role="tabpanel">
             @include('modules.workshopManagement.jobCard.tabs.labour')
         </div>
     </div>
