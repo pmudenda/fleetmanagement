5b3cda4b6227782301ff8c851922eb67a91c27ed
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-04 16:28:28 +0200

    allow addition of permit no

--- resources/views/modules/driverManagement/create.blade.php
+++ resources/views/modules/driverManagement/create.blade.php
@@ -111,7 +111,7 @@
                                                             </div>
                                                         </div>
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
@@ -133,14 +133,14 @@
                                                                 </div>
                                                             </div>
 
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                 </div>
                                                             </div>
                                                         </div>
 
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
@@ -162,7 +162,7 @@
                                                                 </div>
                                                             </div>
 
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row" style="display: none;">
                                                                         <div class="form-group row">
@@ -184,7 +184,7 @@
                                                         </div>
 
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
@@ -209,7 +209,7 @@
                                                         </div>
                                                         <!--Directorate And Department-->
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
@@ -234,7 +234,7 @@
                                                         </div>
 
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
@@ -262,7 +262,7 @@
                                                         </div>
 
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-6 col-md-5">
+                                                            <div class="col-xs-12 col-sm-12 col-md-12">
                                                                 <div class="container-fluid pl-0">
                                                                     <div class="row">
                                                                         <div class="form-group row">
