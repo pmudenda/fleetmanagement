a6bd4c1597c4113bbb251603da767bef716f7423
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-05 19:47:35 +0200

    exit

--- resources/views/modules/workshopManagement/jobCard/exitFromWorkshop.blade.php
+++ resources/views/modules/workshopManagement/jobCard/exitFromWorkshop.blade.php
@@ -592,7 +592,7 @@
                         </div>
                     </section>
 
-                    <h1 class="mt-10">Accessories | Defects | Spares | Services | Labour </h1>
+                    <h1 class="mt-10">Estimated Cost Of Repair</h1>
                     <section>
                         <div class="container-fluid pl-0">
                             <input type="hidden"
