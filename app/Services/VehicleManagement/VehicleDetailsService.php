9e7f5f4103d39c224aba7c389d56b26ca652fb3b
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-05 20:36:42 +0200

    exit

--- app/Services/VehicleManagement/VehicleDetailsService.php
+++ app/Services/VehicleManagement/VehicleDetailsService.php
@@ -47,7 +47,7 @@ class VehicleDetailsService
 
             Log::info("Vehicle Param received $ref");
             $results = DB::table('VM_VEHICLE_HEADER')->
-            where('VM_VEHICLE_HEADER.id', '=', $ref)
+            where('VM_VEHICLE_HEADER.id', '=', '150')
                 //->leftJoin('VM_ENGINE_DETAILS', 'VM_VEHICLE_HEADER.id', '=', 'VM_ENGINE_DETAILS.vehicle_header_id')
                 //->leftJoin('VM_ASSIGNMENTS', 'VM_VEHICLE_HEADER.id', '=', 'VM_ASSIGNMENTS.vehicle_header_id')
                 //->leftJoin('VM_CHASSIS_DETAILS', 'VM_VEHICLE_HEADER.id', '=', 'VM_CHASSIS_DETAILS.vehicle_header_id')
