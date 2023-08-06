72485a6d7d851100c55818fcffd78e3b1fffcaf5
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-06 14:54:13 +0200

    added reminder window

--- resources/views/modules/vehicleManagement/details/view.blade.php
+++ resources/views/modules/vehicleManagement/details/view.blade.php
@@ -12,6 +12,13 @@
             display: inline-block;
             box-shadow: 0px -3px 6px 2px rgba(0, 0, 0, 0.2);
         }
+        .form-control:disabled {
+            border: none !important;
+            background-color: transparent !important;
+        }
+        .bg-gray-300{
+            background-color: transparent !important;
+        }
     </style>
 @endpush
 @section('content')
