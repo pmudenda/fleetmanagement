8932133cc851e54ace58a22564285ee60fb9ae8f
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-07 18:50:19 +0200

    adding labour rates

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Fleet Master:: Login </title>
+    <title>ZESCO FLEET MASTER:: Login </title>
 
     <link rel="shortcut icon" href="{{asset('assets/dist/img/icons/logo.png')}}"
           type="image/x-icon">
