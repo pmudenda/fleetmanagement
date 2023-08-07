128f0c10535d7cac2225aabcd258bfbd79c7a0ca
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-07 13:51:40 +0200

    added mechanics

---
+++ database/migrations/2023_08_07_114737_create_mechanics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wm_mechanics', function (Blueprint $table) {
+            $table->id();
+            $table->string('staff_no', 10);
+            $table->string('name', 255);
+            $table->string('workshop_code', 4);
+            $table->string('section_code', 4);
+            $table->string('status', 2);
+            $table->string('created_by', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wm_mechanics');
+    }
+};
