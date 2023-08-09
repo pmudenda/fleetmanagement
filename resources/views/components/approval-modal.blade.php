0434aa1a1aaf2e063b9e81629368b560e97154af
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-09 13:50:34 +0200

    added totalAmount on work order closure

--- resources/views/components/approval-modal.blade.php
+++ resources/views/components/approval-modal.blade.php
@@ -114,4 +114,127 @@
     </div>
 </div>
 
+<div class="modal fade" id="eSignature-modal" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-md modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="modal-title" id="approvalModalTitle">
+                    <i class="fa fa-pencil-square-o"></i> Approval
+                </div>
+            </div>
+            <form action="{{route('sign.assessment')}}" name="eSignDocument">
+                <input type="hidden" value="{{$details->job_card_no ?? 0}}">
+                <div class="modal-body">
+                    <div>
+                        <div id="approvalDialogSign">
+                            <div style="float:left;">
+                                <span id="spanMessage" style="color: #f00;" class="errorMessage"></span>
+                                <label id="newApproveLblMessage" class="mediumMessage"></label>
+                            </div>
+                            <div style="float:right; padding-left:30px; display: none;">
+                                <input class="small" type="checkbox" id="approveChkSignAs"/>
+                                Sign As Different User...
+                            </div>
+                            {{--   <div id="approveAanDisapprove" style="display: none">
+                                   <label class="app-label">Approve / Disapprove</label>
+                                   <div>
+                                       <span id="spanApproveBtn" class="mr-3">
+                                           <input type="radio"
+                                                  name="optApprove"
+                                                  value="approve"
+                                                  checked="checked"
+                                                  id="approveSelectedPass"/>
+                                           Approve
+                                       </span>
+                                       <span id="spanDisapproveBtn">
+                                           <input type="radio"
+                                                  name="optApprove"
+                                                  value="reject"
+                                                  id="approveSelectedFail"/>
+                                           Disapprove
+                                       </span>
+                                       <span id="spanSendBackBtn">
+                                           <input type="radio"
+                                                  name="optApprove"
+                                                  value="send_back"
+                                                  id="approveSendBack"/>
+                                           Send Back
+                                       </span>
+                                       <br/>
+                                       <br/>
+                                   </div>
+                               </div>--}}
 
+                            <div class="signAsElement">
+                                <label class="app-label field-required app-field-null">Login ID</label>
+                                <div>
+                                    <input class="zqEditMode form-control"
+                                           type="text"
+                                           required
+                                           id="loginIdInput"
+                                           size="25" maxlength="25"/><br/>
+                                </div>
+                            </div>
+                            {{-- <div >
+                                 <label class="app-label field-required app-field-null">Login Password</label>
+                                 <div>
+                                     <input type="password" id="loginPasswordInput"
+                                            class="form-control"
+                                            size="25" maxlength="25"/><br/>
+                                 </div>
+                             </div>--}}
+                            <div class="signAsElement">
+                                <label class="app-label field-required app-field-null">eSignature Password</label>
+                                <div>
+                                    <input type="password"
+                                           required
+                                           class="form-control"
+                                           id="eSignaturePasswordInput" size="25" maxlength="25"/>
+                                </div>
+                            </div>
+                            <div style="clear:both;">
+                                <div>
+                                    <p id="newApproval_Remarks">
+                                        <input
+                                                id="acceptance"
+                                                name="acceptance"
+                                                type="checkbox"
+                                                class="checkbox">
+                                        I hereby, acknowledge that the assessment has been done truthfully and fairly
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="newApproval_DIVWait" style="visibility: hidden; display: none">
+                            <table border="0" style="width:100%; height:100%;">
+                                <tr>
+                                    <td align="center">
+                                        Please wait . . .
+                                        <br/>
+                                        <br/>
+                                        Signature being verified.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-end">
+                    <button id="btnSign" type="button"
+                            class="btn btn-sm btn-success mr-3">
+                        <i class="fas fa-save"></i>
+                        Sign
+                    </button>
+                    <button class="btn btn-sm btn-danger"
+                            type="button"
+                            data-bs-target="#approval-modal"
+                            data-bs-toggle="modal"
+                            data-bs-dismiss="modal">
+                        <i class="fas fa-undo"></i>
+                        Close
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
