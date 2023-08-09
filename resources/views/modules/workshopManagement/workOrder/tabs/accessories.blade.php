e1bffc58f0114b4d5a5442ee2e9f3a617a66f8fd
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-09 16:44:56 +0200

    acknowledgement

--- resources/views/modules/workshopManagement/workOrder/tabs/accessories.blade.php
+++ resources/views/modules/workshopManagement/workOrder/tabs/accessories.blade.php
@@ -173,33 +173,33 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="row">
-                        <table class="table" id="observations">
-                            <thead>
-                            <th>Observation</th>
-                            <th>Attachment</th>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>
-                                    <input type="text" name="observation[]" class="form-control">
-                                </td>
-                                <td>
-                                    <input type="file" name="attachment[]" class="form-control">
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-
-                        <button type="button"
-                                data-table-id="observations"
-                                class="btn btn-sm btn-primary add pull-right"
-                                value="addRow">
-                            <i class="fa fa-plus"></i> Add Row
-                        </button>
+                        <div class="table-responsive" style="max-height:500px;">
+                            <table class="table" id="observations">
+                                <thead>
+                                <th>Observation</th>
+                                <th>Attachment</th>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        <input type="text" name="observation[]" class="form-control">
+                                    </td>
+                                    <td>
+                                        <input type="file" name="attachment[]" class="form-control">
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <button type="button"
+                                    data-table-id="observations"
+                                    class="btn btn-sm btn-primary add pull-right"
+                                    value="addRow">
+                                <i class="fa fa-plus"></i> Add Row
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
 
         </div>
