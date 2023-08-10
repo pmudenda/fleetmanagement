49ef212e71d45a559bf2d259b098d8e287e822a8
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-10 21:12:35 +0200

    adding odometer entry form

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -20,7 +20,7 @@
                             <p>Total Fleet</p>
                         </div>
                         <div class="icon">
-                            <i class="ion fonticon-truck"></i>
+                            <i class="fas fa-truck"></i>
                         </div>
                         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
