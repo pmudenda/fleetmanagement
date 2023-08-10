bfa7ec8e0ab7621643ca79ccc8378f455edb54ef
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-10 16:25:43 +0200

    added

--- resources/views/modules/vehicleManagement/details/view.blade.php
+++ resources/views/modules/vehicleManagement/details/view.blade.php
@@ -732,6 +732,7 @@
                         <div class="tab-pane fade" id="odometerHistory" role="tabpanel">
                             <div class="container-fluid pl-0 mt-5">
                                 Meter History
+                                <div id="example"></div>
                             </div>
                         </div>
 
@@ -926,7 +927,23 @@
             elementsOnCreate.forEach(function (element) {
                 element.style.display = 'none';
                 //('disabled', 'disabled');
-            })
+            });
+
+            const container = document.querySelector('#example');
+
+            const hot = new Handsontable(container, {
+                data: [
+                    ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+                    ['2019', 10, 11, 12, 13],
+                    ['2020', 20, 11, 14, 13],
+                    ['2021', 30, 15, 12, 13]
+                ],
+                rowHeaders: true,
+                colHeaders: true,
+                height: 'auto',
+                licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
+            });
         });
+
     </script>
 @endpush
