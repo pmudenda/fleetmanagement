1da021393ffd8faf711458df386e200c3c374a56
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-11 14:33:31 +0200

    removed sidebar menu

--- resources/views/modules/reports/index.blade.php
+++ resources/views/modules/reports/index.blade.php
@@ -290,7 +290,7 @@
                 if (response.state != 'success') {
                     return;
                 }
-                window.costByYear = response.payload['cost_by_year'];
+                window.costByYear = response.payload['costByYear'];
                 //fuelExpensesByVehicle();
                 //fuelExpenseTotalsByType();
                 fuelExpensesByYear();
