660d5f3397ab262a18a9d5f4a3508055dfd80669
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-13 13:39:41 +0200

    view

--- resources/views/modules/vehicleManagement/details/view.blade.php
+++ resources/views/modules/vehicleManagement/details/view.blade.php
@@ -336,7 +336,14 @@
                     <div class="tab-content">
                         <div class="tab-pane active" id="overview" role="tabpanel">
                             <div class="container-fluid pl-0 mt-5">
-                                Overview
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div id="main" style="height:400px;"></div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div id="pie" style="height:400px;"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
@@ -2956,7 +2963,109 @@
                 }
             }, 600)
 
-        });
+            let myChart =echarts.init(document.getElementById('main'))
+            const option = {
+                xAxis: {
+                    data: ['Animals', 'Fruits', 'Cars']
+                },
+                yAxis: {},
+                dataGroupId: '',
+                animationDurationUpdate: 500,
+                series: {
+                    type: 'bar',
+                    id: 'sales',
+                    data: [
+                        {
+                            value: 5,
+                            groupId: 'animals'
+                        },
+                        {
+                            value: 2,
+                            groupId: 'fruits'
+                        },
+                        {
+                            value: 4,
+                            groupId: 'cars'
+                        }
+                    ],
+                    universalTransition: {
+                        enabled: true,
+                        divideShape: 'clone'
+                    }
+                }
+            };
+            myChart.setOption(option);
+            const drilldownData = [
+                {
+                    dataGroupId: 'animals',
+                    data: [
+                        ['Cats', 4],
+                        ['Dogs', 2],
+                        ['Cows', 1],
+                        ['Sheep', 2],
+                        ['Pigs', 1]
+                    ]
+                },
+                {
+                    dataGroupId: 'fruits',
+                    data: [
+                        ['Apples', 4],
+                        ['Oranges', 2]
+                    ]
+                },
+                {
+                    dataGroupId: 'cars',
+                    data: [
+                        ['Toyota', 4],
+                        ['Opel', 2],
+                        ['Volkswagen', 2]
+                    ]
+                }
+            ];
+            /*myChart.on('click', function (event) {
+                if (event.data) {
+                    var subData = drilldownData.find(function (data) {
+                        return data.dataGroupId === event.data.groupId;
+                    });
+                    if (!subData) {
+                        return;
+                    }
+                    myChart.setOption({
+                        xAxis: {
+                            data: subData.data.map(function (item) {
+                                return item[0];
+                            })
+                        },
+                        series: {
+                            type: 'bar',
+                            id: 'sales',
+                            dataGroupId: subData.dataGroupId,
+                            data: subData.data.map(function (item) {
+                                return item[1];
+                            }),
+                            universalTransition: {
+                                enabled: true,
+                                divideShape: 'clone'
+                            }
+                        },
+                        graphic: [
+                            {
+                                type: 'text',
+                                left: 50,
+                                top: 20,
+                                style: {
+                                    text: 'Back',
+                                    fontSize: 18
+                                },
+                                onclick: function () {
+                                    myChart.setOption(option);
+                                }
+                            }
+                        ]
+                    });
+                }
+            });*/
 
+        });
     </script>
 @endpush
