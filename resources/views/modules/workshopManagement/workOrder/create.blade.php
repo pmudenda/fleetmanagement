46cf977cd047ceb8151f4e33887d5935b2aad994
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-16 13:55:10 +0200

    resolved errors

--- resources/views/modules/workshopManagement/workOrder/create.blade.php
+++ resources/views/modules/workshopManagement/workOrder/create.blade.php
@@ -379,7 +379,7 @@
             </td>
         </tr>
     </template>
-    <div class="modal fade" id="eSignature-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    <div class="modal fade" id="eSignatureModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
          aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
