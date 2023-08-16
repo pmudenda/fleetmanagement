eff16dc97ccacf95658ffe1f459a42ce6fa5ff0f
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-16 21:30:54 +0200

    resolved errors

--- resources/views/modules/workshopManagement/workOrder/create.blade.php
+++ resources/views/modules/workshopManagement/workOrder/create.blade.php
@@ -1099,8 +1099,11 @@
 
                 }, 600);
 
-                $('#labour_table').on('change', '[name="mechanic"]', function () {
+                $('#labour_table').on('change paste', '[name="mechanic"]', function () {
                     const $row = $(this).closest('tr');
+                    if(!this.value || this.value.length < 5){
+                        return;
+                    }
                     findMechanic($row, this.value);
                 });
             });
