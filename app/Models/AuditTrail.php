6bc9f4caaf64a9f99d35612d12ec806c56298f6b
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-19 14:14:05 +0200

    a

--- app/Models/AuditTrail.php
+++ app/Models/AuditTrail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuditTrail extends Model
 {
-    protected $table = '';
+    protected $table = 'gen_audit_trail';
 
     protected $fillable = [
         'event_date',
