924f3464fd5e961572a3e53ab6edd8d57a8dd462
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-19 19:58:32 +0200

    a

--- resources/views/modules/workshopManagement/workOrder/exitFromWorkshop.blade.php
+++ resources/views/modules/workshopManagement/workOrder/exitFromWorkshop.blade.php
@@ -335,7 +335,8 @@
                                                 </div>
                                             </div>
                                         </div>
-
+                                    </div>
+                                    <div class="row">
                                         <div class="col-xs-12 col-sm-6 col-md-6">
                                             <div class="container-fluid pl-0">
                                                 <div class="row">
@@ -351,6 +352,27 @@
                                                                     id="fuel_level"
                                                                     class="form-select form-select-sm when_valid"
                                                                     required>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-6 col-md-6">
+                                            <div class="container-fluid pl-0">
+                                                <div class="row">
+                                                    <div class="form-group row">
+                                                        <label
+                                                                class="col-xs-12 col-sm-12 col-md-5 col-lg-4"
+                                                                for="sub_fuel_level">
+                                                            Sub-Tank Fuel Level :
+                                                        </label>
+                                                        <div class="col-xs-12 col-sm-6 col-md-7 col-lg-7">
+                                                            <select name="sub_fuel_level"
+                                                                    data-value="{{$details->sub_fuel_level_in ?? ''}}"
+                                                                    id="sub_fuel_level"
+                                                                    class="form-select form-select-sm when_valid">
                                                             </select>
                                                         </div>
                                                     </div>
