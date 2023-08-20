c77c537c074600da1f85ffac94ae2d67aa86f9ee
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-20 14:52:01 +0200

    workshop updates

--- resources/views/modules/workshopManagement/workOrder/tabs/services.blade.php
+++ resources/views/modules/workshopManagement/workOrder/tabs/services.blade.php
@@ -316,7 +316,7 @@
                                 <td>
                                     <input name="service_unit_price"
                                            required
-                                           value="{{$service->unit_of_measure ?? ''}}"
+                                           value="{{$service->price ?? $service->amount_est}}"
                                            readonly
                                            class="form-control form-control-sm service_unit_price"/>
                                 </td>
