e8be0fc0f966e96d3f658b121efa5ab54a76e4b7
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-21 08:37:36 +0200

    labour assignment not available for contracted services

--- app/Http/Controllers/Documents/DocumentController.php
+++ app/Http/Controllers/Documents/DocumentController.php
@@ -7,54 +7,60 @@ use App\Models\Reference\DocumentFollowup;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DocumentController extends Controller
 {
     public function documentFollowup(Request $request): JsonResponse
     {
-        $query = DocumentFollowup::query();
 
-        if ($request->has('documentType') && $request->filled('documentType')) {
-            $query->where(function ($query) use ($request) {
-                $query->where("type_document", "=", strtoupper(trim($request->get('documentType'))));
-            });
+        try {
+            $query = DocumentFollowup::query();
+            if ($request->has('documentType') && $request->filled('documentType')) {
+                $query->where(function ($query) use ($request) {
+                    $query->where("type_document", "=", strtoupper(trim($request->get('documentType'))));
+                });
+            }
+            if ($request->has('documentNumber') && $request->filled('documentNumber')) {
+                $query->where(function ($query) use ($request) {
+                    $query->where("document_no", "=", strtoupper(trim($request->get('documentNumber'))));
+                });
+            }
+            if ($request->has('periodFrom')
+                && $request->has('periodTo')
+                && $request->filled('periodFrom')
+                && $request->filled('periodTo')
+            ) {
+                $query->where(function ($query) use ($request) {
+                    $startDate = Carbon::createFromFormat('Y-m-d', $request->get('periodFrom'));
+                    $endDate = Carbon::createFromFormat('Y-m-d', $request->get('periodTo'));
+                    $query->whereBetween('date_act', [$startDate, $endDate]);
+                });
+            } elseif ($request->has('periodFrom') && $request->filled('periodFrom')) {
+
+                $query->where(function ($query) use ($request) {
+                    $startDate = Carbon::createFromFormat('Y-m-d', $request->get('periodFrom'));
+                    $query->whereDate('date_act', '>=', $startDate);
+                });
+
+            } elseif ($request->has('periodTo') && $request->filled('periodTo')) {
+                $query->where(function ($query) use ($request) {
+                    $endDate = Carbon::createFromFormat('Y-m-d', $request->get('periodTo'));
+                    $query->whereDate('date_act', '<=', $endDate);
+                });
+
+            }
+            return response()->json([
+                'state' => 'success',
+                'payload' => $query->get()
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json([
+                'state' => 'failure',
+                'payload' => []
+            ]);
         }
-
-        if ($request->has('documentNumber') && $request->filled('documentNumber')) {
-            $query->where(function ($query) use ($request) {
-                $query->where("document_no", "=", strtoupper(trim($request->get('documentNumber'))));
-            });
-        }
-
-        if ($request->has('periodFrom')
-            && $request->has('periodTo')
-            && $request->filled('periodFrom')
-            && $request->filled('periodTo')
-        ) {
-            $query->where(function ($query) use ($request) {
-                $startDate = Carbon::createFromFormat('Y-m-d', $request->get('periodFrom'));
-                $endDate = Carbon::createFromFormat('Y-m-d', $request->get('periodTo'));
-                $query->whereBetween('date_act', [$startDate, $endDate]);
-            });
-        } elseif ($request->has('periodFrom') && $request->filled('periodFrom')) {
-
-            $query->where(function ($query) use ($request) {
-                $startDate = Carbon::createFromFormat('Y-m-d', $request->get('periodFrom'));
-                $query->whereDate('date_act', '>=', $startDate);
-            });
-
-        } elseif ($request->has('periodTo') && $request->filled('periodTo')) {
-            $query->where(function ($query) use ($request) {
-                $endDate = Carbon::createFromFormat('Y-m-d', $request->get('periodTo'));
-                $query->whereDate('date_act', '<=', $endDate);
-            });
-
-        }
-
-        return response()->json([
-            'state' => 'success',
-            'payload' => $query->get()
-        ]);
 
     }
 
