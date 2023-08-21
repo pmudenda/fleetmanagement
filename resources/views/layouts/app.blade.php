b208282a33fd6311b837780af64544c940202dd3
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-21 10:12:37 +0200

    labour assignment not available for contracted services

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -544,7 +544,7 @@
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
                     type: 'GET',
-                    dataType: 'json',
+                    dataType: 'html',
                     data: postData
                 };
 
