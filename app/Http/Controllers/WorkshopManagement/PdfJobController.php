c04e4f7a05bfb8a383855f4adb00b4ed821a1e84
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-22 14:42:54 +0200

    show different button for incomplete job card

--- app/Http/Controllers/WorkshopManagement/PdfJobController.php
+++ app/Http/Controllers/WorkshopManagement/PdfJobController.php
@@ -2,49 +2,11 @@
 
 namespace App\Http\Controllers\WorkshopManagement;
 
-use GuzzleHttp\Client;
-use Illuminate\Http\Request;
-use Codedge\Fpdf\Fpdf\Fpdf;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-//
+use Codedge\Fpdf\Fpdf\Fpdf;
 
 class PdfJobController extends Controller
 {
-    public function index_old()
-    {
-        $this->fpdf->SetFont('Arial', 'B', 15);
-        $this->fpdf->AddPage("L", ['100', '100']);
-        //$this->fpdf->Text(10, 10, "Cintent");
-
-        // Column widths
-        $w = array(40, 35, 40, 45);
-        $header = array('Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)');
-
-        $data = [
-            ['Zambia','Lusaka',50,18000000]
-        ];
-        // Header
-        for($i=0;$i<count($header);$i++)
-            $this->fpdf->Cell($w[$i],7,$header[$i],1,0,'C');
-        $this->fpdf->Ln();
-        // Data
-        foreach($data as $row)
-        {
-            $this->fpdf->Cell($w[0],6,$row[0],'LR');
-            $this->fpdf->Cell($w[1],6,$row[1],'LR');
-            $this->fpdf->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-            $this->fpdf->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-            $this->fpdf->Ln();
-        }
-        // Closing line
-        $this->fpdf->Cell(array_sum($w),0,'','T');
-
-        $this->fpdf->Output();
-
-        exit;
-    }
-
     protected $fpdf;
     private $errorMessage;
 
@@ -58,20 +20,17 @@ class PdfJobController extends Controller
     {
         // read customer info from data store
         $customer = [
-            'service_no'=> 333333,
+            'service_no' => 333333,
             'meter_number' => 6666,
             'customer_name' => 'Customer',
             'address' => 'Address',
             'tarrif' => 'Tarrif'
         ];
         //self::getCustomerDetails($id); // Get Customer Details for the Statement
-        $service_no = $customer["service_no"];
-        $meter_number = $customer["meter_number"];
-        $customer_name = $customer["customer_name"];
-        $address = $customer["address"];
-        $tariff_name = $customer["tarrif"];
 
-        $bill_details = ['data'=> []];// $this->getStatements($service_no);
+        $customer_name = $customer["customer_name"];
+
+        $bill_details = ['data' => []];// $this->getStatements($service_no);
 
         $filepath = public_path() . 'assets/dist/img/zesco_logo.png'; //Image for Statement and Invoice
 
@@ -84,7 +43,7 @@ class PdfJobController extends Controller
 
         $label_size = '12';
         $text_size = '12';
-
+        $this->fpdf->SetTitle($filepath);
         // set font to arial, regular, 12pt
         $this->fpdf->SetFont('Arial', '', 12);
         // ----------------------------------------------------
@@ -220,16 +179,16 @@ class PdfJobController extends Controller
 
         //make a dummy empty cell as a vertical spacer
         //-------------------------------------------------------------------------------------------------------
-                $this->fpdf->Cell(189, 10, '', 0, 1);//end of line
+        $this->fpdf->Cell(189, 10, '', 0, 1);//end of line
         //-------------------------------------------------------------------------------------------------------
 
         //invoice contents
 
-                $this->fpdf->SetFillColor(28, 153, 85);
+        $this->fpdf->SetFillColor(28, 153, 85);
         //        $this->fpdf->SetDrawColor(28, 153, 85);
         //$pdf->SetLineWidth(.3);
-                $this->fpdf->SetFont('Arial', 'B', 12);
-                $this->fpdf->SetTextColor(255, 255, 255);
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->SetTextColor(255, 255, 255);
         //-------------------------------------------------------------------------------------------------------
 
 
@@ -245,59 +204,59 @@ class PdfJobController extends Controller
         $this->fpdf->SetFont('Arial', 'B', $label_size);
         $this->fpdf->Cell(60, 5, "A. DEMAND CHARGES");
         $this->fpdf->Ln();
-        $this->fpdf->Cell(120, 5, '1. Contracted Capacity' , 1, 0);
+        $this->fpdf->Cell(120, 5, '1. Contracted Capacity', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'7,672', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '7,672', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '2. Actual Maximum Demand (Kva)' , 1, 0);
+        $this->fpdf->Cell(120, 5, '2. Actual Maximum Demand (Kva)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'11,730', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '11,730', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
         $this->fpdf->Ln();
 
-        $this->fpdf->Cell(120, 5, '3. Multiplication Factor' , 1, 0);
+        $this->fpdf->Cell(120, 5, '3. Multiplication Factor', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'1.00', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '1.00', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '4. Chargeable Maximum Demand (Kva)' , 1, 0);
+        $this->fpdf->Cell(120, 5, '4. Chargeable Maximum Demand (Kva)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'-', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '-', 1, '');//end of line
 
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '5. Tariff' , 1, 0);
+        $this->fpdf->Cell(120, 5, '5. Tariff', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'-', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '-', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
         $this->fpdf->Ln();
 
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '6. Demand Charge @0.00 US$/Kva' , 1, 0);
+        $this->fpdf->Cell(120, 5, '6. Demand Charge @0.00 US$/Kva', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'-', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '-', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
         $this->fpdf->Ln();
         $this->fpdf->Ln();
@@ -307,80 +266,80 @@ class PdfJobController extends Controller
         $this->fpdf->SetFont('Arial', 'B', $label_size);
         $this->fpdf->Cell(60, 5, "B. ENERGY CHARGES");
         $this->fpdf->Ln();
-        $this->fpdf->Cell(120, 5, '1. Present Meter Reading (kWh)' , 1, 0);
+        $this->fpdf->Cell(120, 5, '1. Present Meter Reading (kWh)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'256,932,280', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '256,932,280', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '2. Previous Meter Reading' , 1, 0);
+        $this->fpdf->Cell(120, 5, '2. Previous Meter Reading', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'252,057,060', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '252,057,060', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
         $this->fpdf->Ln();
 
-        $this->fpdf->Cell(120, 5, '3. Units Consumed (kWh)' , 1, 0);
+        $this->fpdf->Cell(120, 5, '3. Units Consumed (kWh)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'4,875,220', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '4,875,220', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '4. Multiplication Factor' , 1, 0);
+        $this->fpdf->Cell(120, 5, '4. Multiplication Factor', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'1.00', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '1.00', 1, '');//end of line
 
 
         $this->fpdf->Ln();
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '5. Chargeable Units (kWh)' , 1, 0);
+        $this->fpdf->Cell(120, 5, '5. Chargeable Units (kWh)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'4,875,220', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '4,875,220', 1, '');//end of line
 //-------------------------------------------------------------------------------------------------------
         $this->fpdf->Ln();
 
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '6. Rate per unit US$/kWh' , 1, 0);
+        $this->fpdf->Cell(120, 5, '6. Rate per unit US$/kWh', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'0.1100', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '0.1100', 1, '');//end of line
 
         $this->fpdf->Ln();
 
         //-------------------------------------------------------------------------------------------------------
 
-        $this->fpdf->Cell(120, 5, '7. Energy Charge' , 1, 0);
+        $this->fpdf->Cell(120, 5, '7. Energy Charge', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'536,274.20', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '536,274.20', 1, '');//end of line
 
 
         $this->fpdf->Ln();
         $this->fpdf->Ln();
 
 
-        $this->fpdf->Cell(120, 5, 'TOTAL VALUE OF INVOICE (US$)' , 1, 0);
+        $this->fpdf->Cell(120, 5, 'TOTAL VALUE OF INVOICE (US$)', 1, 0);
 //            $this->fpdf->Cell(47, 5, $item['period'], 1, 0);
 //            $this->fpdf->Cell(35, 5, "ZMW " . $item['amount'], 1, 0);
 //            $this->fpdf->Cell(38, 5, "ZMW " . $item['balance'], 1, 0);
-        $this->fpdf->Cell(70, 5,'536,274.20', 1, '');//end of line
+        $this->fpdf->Cell(70, 5, '536,274.20', 1, '');//end of line
 
         $this->fpdf->Ln();
         $this->fpdf->Ln();
@@ -393,7 +352,6 @@ class PdfJobController extends Controller
 //        $this->fpdf->Cell(50, 5, '05-2023', 0, 1);
         $this->fpdf->Ln();
         $this->fpdf->Ln();
-
 
 
         $this->fpdf->SetFont('Arial', 'B', $label_size);
@@ -420,7 +378,6 @@ class PdfJobController extends Controller
         $this->fpdf->Ln(2);
 
 
-
         $this->fpdf->SetFont('Arial', 'B', $label_size);
         $this->fpdf->Cell(50, 5, "Branch Name:");
         $this->fpdf->SetFont('Arial', '', $text_size);
@@ -435,7 +392,6 @@ class PdfJobController extends Controller
         $this->fpdf->SetTextColor(0, 0, 0);
         $this->fpdf->Cell(50, 5, 'SCBLZMLX', 0, 1);
         $this->fpdf->Ln(2);
-
 
 
         $this->fpdf->Ln();
