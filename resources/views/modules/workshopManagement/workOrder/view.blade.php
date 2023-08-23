0979af2193f81fc626604f86f15f636643681538
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-23 16:11:10 +0200

    body type clean up

--- resources/views/modules/workshopManagement/workOrder/view.blade.php
+++ resources/views/modules/workshopManagement/workOrder/view.blade.php
@@ -2427,8 +2427,9 @@
             $('[value="insertRow"]').off('click').attr('disabled', true).addClass('d-none');
             $('[name="saveDefects"]').off('click').attr('disabled', true).addClass('d-none');
 
-            $("#labour_table")('.dropdown-toggle').off('click').attr('disabled', true).addClass('d-none');
-            $("#labour_table")('[input[type="checkbox"]]').attr('disabled', true);
+            $("#labour_table").find('.dropdown-toggle').off('click').attr('disabled', true).addClass('d-none');
+            $("#labour_table").find('input[type="checkbox"]').attr('disabled', true);
+            $('button[value="deleteRow"]').addClass('d-none').attr('disabled', true);
         }
 
         $(document).ready(function () {
