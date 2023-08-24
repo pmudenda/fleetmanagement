acaa1b46798f9ad28966430f124df5da9171e811
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-24 18:30:08 +0200

    added method to get reservations only

--- resources/views/modules/workshopManagement/workOrder/create.blade.php
+++ resources/views/modules/workshopManagement/workOrder/create.blade.php
@@ -369,7 +369,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" id="reservedMaterialsContent">
-                    <table class="table table-bordered">
+                    <table class="table table-bordered" id="reservedMaterialsTable">
                         <thead>
                         <td><input type='checkbox'
                                    name='reservedMaterial'
