a2eb9da2e9d03a709e1792f2af5fc76ee98927e2
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-25 18:53:17 +0200

    remove code smells

--- resources/views/modules/workshopManagement/workOrder/create.blade.php
+++ resources/views/modules/workshopManagement/workOrder/create.blade.php
@@ -422,7 +422,7 @@
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Reserved Materials</h5>
+                    <h5 class="modal-title text-danger" id="exampleModalLabel">Job Card Has Reserved Materials</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" id="reservedMaterialsContent">
