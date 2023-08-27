05dfae0a8c753121c2c719da8eb89d53e33a066e
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-27 17:13:41 +0200

    adding mechanic view

--- resources/views/modules/mechanicManagement/list.blade.php
+++ resources/views/modules/mechanicManagement/list.blade.php
@@ -1,4 +1,4 @@
-@php use Carbon\Carbon; @endphp
+@php @endphp
 @extends('layouts.app')
 
 
@@ -37,8 +37,8 @@
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                          xmlns="http://www.w3.org/2000/svg">
                                         <path
-                                            d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
-                                            fill="currentColor"></path>
+                                                d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
+                                                fill="currentColor"></path>
                                     </svg>
                                 </span>
                                     Filter
@@ -58,20 +58,19 @@
                                     <tr>
                                         <th>#</th>
                                         <th>Image</th>
-                                        <th>Driver Name</th>
+                                        <th>Name</th>
                                         <th>Staff Number</th>
-                                        <th>License No</th>
-                                        <th>License Expiry Date</th>
-                                        <th>Permit No</th>
-                                        <th>Permit Expiry</th>
+                                        <th>Workshop</th>
+                                        <th>Section</th>
+                                        <th>Is Head Of Workshop</th>
                                         <th>Status</th>
                                         {{--@can(config('rights.user_show'))--}}
-                                        {{--<th>Action</th>--}}
-                                        {{--@endcan--}}
+                                        <th>Action</th>
+                                        {{-- @endcan--}}
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($mechanics as $key => $user)
+                                    @foreach($mechanics as $key =>$mechanic)
                                         <tr>
                                             <td>
                                                 {{++$key}}
@@ -80,7 +79,7 @@
                                                 @if(!empty($user->avatar))
                                                     <img class="profile-user-img img-fluid img-circle border-0"
                                                          width="100%"
-                                                         src="{{ asset('storage/user_avatar/' . $user->avatar) }}"
+                                                         src='{{ asset("storage/user_avatar/$mechanic->avatar")}}'
                                                          alt="Image not found"
                                                          style="width: 60px; height: 54px;"
                                                     />
@@ -97,23 +96,20 @@
                                                 {{$user->name}}
                                             </td>
                                             <td>
-                                                {{$user->staff_number ?? '--'}}
+                                                {{$user->staff_no ?? '--'}}
                                             </td>
 
                                             <td>
-                                                {{$user->license_number ?? '--'}}
+                                                {{$mechanic->license_number ?? '--'}}
                                             </td>
                                             <td>
-                                                {{Carbon::parse($user->license_date_expiry)->format('d/m/Y') ?? '--'}}
+                                                {{'--'}}
                                             </td>
                                             <td>
-                                                {{$user->permit_number ?? '--'}}
+                                                {{'--'}}
                                             </td>
                                             <td>
-                                                {{Carbon::parse($user->license_date_expiry)->format('d/m/Y') ?? '--'}}
-                                            </td>
-                                            <td>
-                                                @if($user->status == '01')
+                                                @if($mechanic->status == '01')
                                                     <span class="badge badge-success">
                                                     Active
                                                 </span>
@@ -123,14 +119,14 @@
                                                 </span>
                                                 @endif
                                             </td>
-                                            {{--@can(config('rights.user_show'))--}}
-                                            {{--<td>
-                                                <a href="{{route('driver.show', $user->id)}}"
+                                            {{--   @can(config('rights.user_show'))--}}
+                                            <td>
+                                                <a href="{{URL::signedRoute('mechanic.show',$mechanic->id)}}"
                                                    class="btn btn-sm btn-success m-1">
                                                     <i class="fas fa-eye">Details</i>
                                                 </a>
-                                            </td>--}}
-                                            {{-- @endcan--}}
+                                            </td>
+                                            {{--@endcan--}}
                                         </tr>
                                     @endforeach
                                     </tbody>
