bc7c48b44a2f94d89214d766e4e10851e3d7d4d5
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-27 11:47:25 +0200

    fuel management table

--- database/migrations/2023_07_24_140549_create_fuel_management_table.php
+++ database/migrations/2023_07_24_140549_create_fuel_management_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -10,35 +11,97 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('VM_FUEL_ISSUE', function (Blueprint $table) {
-            $table->id();
-            $table->string('created_by', 20);
-            $table->string('voucher_no', 20);
-            $table->timestamp('voucher_date');
-            $table->timestamp('voucher_time');
+        Schema::create('FUEL_MANAGEMENT', function (Blueprint $table) {
+            //$table->id();
+            $table->string('user_act', 20)->nullable();;
+            $table->date('fech_act')->nullable();
+            $table->string('voucher_no', 20)->nullable();
             $table->string('document_no', 20)->nullable();
-            $table->string('reg_no', 10);
-            $table->string('cost_center', 12);
-            $table->string('area_code', 2);
-            $table->string('authorised_by', 50);
-            $table->string('received_by', 50);
-            $table->string('issue_office', 50);
-            $table->string('unit_of_measure', 6);
-            $table->string('fuel_code', 12);
+            $table->string('reg_no', 20);
+            $table->string('cost_center', 12)->nullable();
+            $table->date('voucher_date')->nullable();
+            $table->string('bus_area', 2)->nullable();
+            $table->string('authorisor', 50)->nullable();
+            $table->string('receiver', 50)->nullable();
+            $table->string('issue_office', 50)->nullable();
+            $table->string('unit_measure', 6)->nullable();
+            $table->string('fuel_code', 12)->nullable();
             $table->decimal('quantity', 19, 4)->nullable();
             $table->decimal('price', 19, 4)->nullable();
             $table->decimal('amount', 19, 4)->nullable();
-            $table->integer('odometer')->nullable();
-            $table->decimal('pump_start')->nullable();
-            $table->decimal('pump_end')->nullable();
-            $table->string('status', 15);
+            $table->decimal('odometer', 19, 0)->nullable();
+            $table->decimal('pump_start', 19, 0)->nullable();
+            $table->decimal('pump_end', 19, 0)->nullable();
+            $table->string('status', 15)->nullable();
+            $table->date('voucher_time')->nullable();
             $table->string('business_unit', 8)->nullable();
             $table->string('user_unit', 15)->nullable();
-            $table->string('system_origin', 10)->nullable();
-            $table->string('requisition_type', 2)->nullable();
-            $table->string('justification', 2000)->nullable();
+            $table->string('sys_origin', 10)->nullable();
+            $table->string('fueling_type', 2)->nullable();
+            $table->string('comments', 2000)->nullable();
+            $table->unique(["VOUCHER_NO", "REG_NO", "STATUS", "VOUCHER_DATE"]);
             $table->timestamps();
         });
+
+        DB::unprepared('CREATE INDEX "IDX_FUELMNGT" ON "FUEL_MANAGEMENT" ("REG_NO", "FUELING_TYPE", "VOUCHER_DATE")');
+        DB::unprepared('CREATE INDEX "INDFMBA" ON "FUEL_MANAGEMENT" ("BUS_AREA")');
+        DB::unprepared('CREATE INDEX "INDFMVD" ON "FUEL_MANAGEMENT" ("FECH_ACT")');
+        DB::unprepared('CREATE UNIQUE INDEX "IND_FM_DOC_NO" ON "FUEL_MANAGEMENT" ("DOCUMENT_NO")');
+        DB::unprepared('CREATE INDEX "IND_VD" ON "FUEL_MANAGEMENT" ("VOUCHER_DATE")');
+        DB::unprepared('CREATE OR REPLACE TRIGGER "TR_FUEL_MANAGEMENT_INSERT"
+                                AFTER INSERT
+                                ON FUEL_MANAGEMENT
+                                REFERENCING NEW AS NEW OLD AS OLD
+                                FOR EACH ROW
+                                DECLARE
+                                LN_COUNT NUMBER;
+                                LN_OLD_MILLEAGE NUMBER;
+
+                                vdt fuel_management.voucher_date%type;
+                                BEGIN
+
+                                    UPDATE vm_vehicle_header
+                                    SET MILEAGE = :NEW.ODOMETER
+                                    WHERE TRIM(REGISTRATION_NUMBER)=TRIM(:NEW.REG_NO);
+                                END;
+                                /
+                                ALTER TRIGGER "TR_FUEL_MANAGEMENT_INSERT" ENABLE;
+
+                                CREATE OR REPLACE TRIGGER "TR_FUEL_ODOMETER_UPDATE"
+                                AFTER UPDATE
+                                OF ODOMETER
+                                ON FUEL_MANAGEMENT
+                                REFERENCING NEW AS NEW OLD AS OLD
+                                FOR EACH ROW
+                                DECLARE
+                                LN_COUNT NUMBER;
+                                BEGIN
+                                UPDATE vm_vehicle_header
+                                SET MILEAGE = :NEW.ODOMETER
+                                WHERE trim(REGISTRATION_NUMBER)= trim(:OLD.REG_NO);
+                                END;
+                                /
+                                ALTER TRIGGER "TR_FUEL_ODOMETER_UPDATE" ENABLE;
+
+                                GRANT UPDATE ON "FUEL_MANAGEMENT" TO "ORAFINANCE";
+
+                                GRANT SELECT ON "FUEL_MANAGEMENT" TO "ORAFINANCE";
+
+                                GRANT INSERT ON "FUEL_MANAGEMENT" TO "ORAFINANCE";
+
+                                GRANT UPDATE ON "FUEL_MANAGEMENT" TO "END_USER_TMS";
+
+                                GRANT SELECT ON "FUEL_MANAGEMENT" TO "END_USER_TMS";
+
+                                GRANT INSERT ON "FUEL_MANAGEMENT" TO "END_USER_TMS";
+
+                                GRANT DELETE ON "FUEL_MANAGEMENT" TO "END_USER_TMS";
+
+                                GRANT UPDATE ON "FUEL_MANAGEMENT" TO "SPMS";
+
+                                GRANT SELECT ON "FUEL_MANAGEMENT" TO "SPMS";
+
+GRANT INSERT ON "FUEL_MANAGEMENT" TO "SPMS";');
     }
 
     /**
@@ -46,6 +109,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('VM_FUEL_ISSUE');
+        Schema::dropIfExists('FUEL_MANAGEMENT');
     }
 };
