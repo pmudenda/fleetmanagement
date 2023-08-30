6522ddcd5960a6f38db73bbac0d2aa4abf45f88b
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-30 13:33:32 +0200

    added closed job option

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -117,7 +117,7 @@
                                 <div>
                                     <i class="fas fa fa-caret-right text-success"></i>
                                     <a href="#">Maintenance Requisition<span
-                                            class="float-right"><strong>0</strong></span></a>
+                                                class="float-right"><strong>0</strong></span></a>
                                 </div>
                                 <div>
                                     <i class="fas fa fa-caret-right text-success"></i>
@@ -198,7 +198,8 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table id="listTable"
+                                <table aria-label="tasks table"
+                                       id="listTable"
                                        class="table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer">
                                     <thead>
                                     <tr>
@@ -232,11 +233,9 @@
                                                 {{Carbon::parse($rec->date_acted)->format('d/m/Y')}}
                                             </td>
                                             <td>
-                                                {{--'show.fuel.requisition'--}}
                                                 <a href="{{URL::signedRoute($rec->url,['ref'=> $rec->reference])}}"
                                                    class="btn btn-sm btn-success">
-                                                    <i class="fas fa-eye"></i>
-                                                    Open
+                                                    Details
                                                 </a>
                                             </td>
 
@@ -268,8 +267,15 @@
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                          xmlns="http://www.w3.org/2000/svg">
                                         <path
-                                            d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
-                                            fill="currentColor"></path>
+                                                d="M19.0759 3H4.72777C3.95892 3
+                                            3.47768 3.83148 3.86067 4.49814L8.56967
+                                            12.6949C9.17923 13.7559 9.5 14.9582 9.5
+                                            16.1819V19.5072C9.5 20.2189 10.2223
+                                            20.7028 10.8805 20.432L13.8805 19.1977C14.2553
+                                            19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5
+                                            12.8089 14.8171 11.8056 15.408 10.964L19.8943
+                                            4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
+                                                fill="currentColor"></path>
                                     </svg>
                                 </span>
                                     Filter
