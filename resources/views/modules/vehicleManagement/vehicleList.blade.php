cd32f71ecc0da4d762d95f24e0318c047f5633f0
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-08-30 13:41:10 +0200

    added closed job option

--- resources/views/modules/vehicleManagement/vehicleList.blade.php
+++ resources/views/modules/vehicleManagement/vehicleList.blade.php
@@ -44,7 +44,7 @@
                             </h2>
                         </div>
 
-                        <div id="collapseOne" style="display: none;" class="collapse" aria-labelledby="headingOne"
+                        <div id="collapseOne"  class="collapse" aria-labelledby="headingOne"
                              data-parent="#accordionExample">
                             <div class="card-body px-0">
                                 <div class="listqbe">
