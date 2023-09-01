6aa5d26f5106bc217fe109abbcb09a2cfe615430
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-01 17:14:40 +0200

    accident reporting

--- database/migrations/2023_03_18_091148_create_vehicle_headers_table.php
+++ database/migrations/2023_03_18_091148_create_vehicle_headers_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('VM_VEHICLE_HEADER', function (Blueprint $table){
             $table->id();
+            $table->string('registration_number', 10);
             $table->string('brand_code',2)->nullable();
             $table->string('brand_name')->nullable();
             $table->string('model_name')->nullable();
@@ -20,18 +21,19 @@ return new class extends Migration {
             $table->string('body_type_name')->nullable();
             $table->string('barcode', 255)->nullable();
             $table->string('has_tom_card', 1)->default('N');
-            $table->string('on_boarding_status', 4)->nullable();
+
             $table->string('status', 4)->nullable();
             $table->decimal('mileage', 19, 4)->nullable();
-
-            $table->string('registration_number', 10);
-            $table->string('business_unit_code');
-            $table->string('business_unit_name');
-            $table->integer('invalid_odometer_entry');
-            $table->string('location_code', 200);
-            $table->string('location_name', 100)->nullable();
             $table->string('registration_type', 4);
 
+            $table->string('business_unit_code');
+            $table->string('business_unit_name');
+
+            $table->string('location_code', 200);
+            $table->string('location_name', 100)->nullable();
+
+            $table->integer('invalid_odometer_entry');
+            $table->string('on_boarding_status', 4)->nullable();
             $table->string('created_by');
             $table->string('created_name');
             $table->timestamp('deleted_at')->nullable();
