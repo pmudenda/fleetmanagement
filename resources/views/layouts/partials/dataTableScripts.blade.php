298eabeb043d46b147b9907f9568657699910332
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-08 14:05:18 +0200

    unified response format

--- resources/views/layouts/partials/dataTableScripts.blade.php
+++ resources/views/layouts/partials/dataTableScripts.blade.php
@@ -10,4 +10,4 @@
 <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/datatables/searchbuilder/dataTables.searchBuilder.js')}}"></script>
+<script src="{{ asset('assets/plugins/datatables/searchbuilder/dataTables.searchBuilder.min.js')}}"></script>
