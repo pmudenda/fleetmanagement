8813457b2a2a9675db9b877140de3b498c0cb073
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-08 13:44:50 +0200

    clean bugs

--- app/Http/Controllers/Security/RolesController.php
+++ app/Http/Controllers/Security/RolesController.php
@@ -94,7 +94,7 @@ class RolesController extends Controller
             Log::info("Role Name  $request->name");
             Log::info("Role Id  $request->id");
 
-            $role = Role::where('id', '=', $request->id)->first();
+            $role = Role::where('id', '=', (int)$request->id)->first();
             if(empty($role)){
                 throw new DataNotFoundException("Role Not Found");
             }
