e5adb3b565d583f798932195ee6a861c93da2035
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-11 18:54:37 +0200

    profile assignment

--- resources/views/modules/security/roles/show.blade.php
+++ resources/views/modules/security/roles/show.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 @push('styles')
     <!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('dashboard/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet"
-          href="{{ asset('dashboard/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('dashboard/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
 @endpush
 
 @section('content')
