9dc54136c66099fcf338d21c669bc21479b0d8fa
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-19 13:25:26 +0200

    a

--- app/Models/Security/User.php
+++ app/Models/Security/User.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasPermissions;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
