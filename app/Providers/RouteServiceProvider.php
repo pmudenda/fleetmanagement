32964002c4d1078d79444b77e9b2991341646002
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-20 10:41:16 +0200

    uncomment fuel.php route file in routeservice provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -54,8 +54,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/data.php'));
 
-            /*Route::middleware('web')
-                 ->group(base_path('routes/fuel.php'));*/
+            Route::middleware('web')
+                ->group(base_path('routes/fuel.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/configuration.php'));
