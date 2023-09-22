e48f988395dc61efd5a7b105f401d7a9e94453d1
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-22 10:38:05 +0200

    Implement Liskov Subsitition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,16 @@
 namespace App\Providers;
 
 
+use App\Interfaces\VehicleManagement\FitnessService;
+use App\Interfaces\VehicleManagement\InsuranceService;
+use App\Interfaces\VehicleManagement\RoadTaxService;
 use App\Interfaces\VehicleManagement\VehicleDetailsService;
 use App\Services\FileUploads\FileUploadService;
 use App\Services\Logging\ActivityLogsService;
+use App\Services\VehicleManagement\FitnessServiceImpl;
+use App\Services\VehicleManagement\InsuranceServiceImpl;
 use App\Services\VehicleManagement\OnBoarding\OnBoardingService;
+use App\Services\VehicleManagement\RoadTaxServiceImpl;
 use App\Services\VehicleManagement\VehicleDetailsServiceImpl;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
@@ -23,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(FileUploadService::class, FileUploadService::class);
         $this->app->bind(ActivityLogsService::class, ActivityLogsService::class);
         $this->app->bind(VehicleDetailsService::class, VehicleDetailsServiceImpl::class);
+        $this->app->bind(InsuranceService::class, InsuranceServiceImpl::class);
+        $this->app->bind(FitnessService::class, FitnessServiceImpl::class);
+        $this->app->bind(RoadTaxService::class, RoadTaxServiceImpl::class);
     }
 
     /**
