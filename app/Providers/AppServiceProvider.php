f7605f1bfe2997ee6d4789d68dba3db4afacec26
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-22 10:35:48 +0200

    activity

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 
+use App\Interfaces\VehicleManagement\VehicleDetailsService;
 use App\Services\FileUploads\FileUploadService;
 use App\Services\Logging\ActivityLogsService;
 use App\Services\VehicleManagement\OnBoarding\OnBoardingService;
+use App\Services\VehicleManagement\VehicleDetailsServiceImpl;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(OnBoardingService::class, OnBoardingService::class);
         $this->app->bind(FileUploadService::class, FileUploadService::class);
         $this->app->bind(ActivityLogsService::class, ActivityLogsService::class);
+        $this->app->bind(VehicleDetailsService::class, VehicleDetailsServiceImpl::class);
     }
 
     /**
