3f4de149252fcfd7f3e6c152184aafdfb14902dc
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-25 19:08:46 +0200

    imprest alert

--- resources/views/modules/workshopManagement/workOrder/tabs/imprest_buy.blade.php
+++ resources/views/modules/workshopManagement/workOrder/tabs/imprest_buy.blade.php
@@ -422,8 +422,7 @@
                                                        class="form-control form-control-sm"
                                                        readonly
                                                        id="pettyCashStoreName"
-                                                       value="{{$officeDetails->store_code ?? ''}}:
-                                                       {{$officeDetails->store_name ?? ''}}"
+                                                       value="{{$officeDetails->store_code ?? ''}}:{{$officeDetails->store_name ?? ''}}"
                                                        placeholder=""
                                                        name="pettyCashStoreName"/>
                                             </div>
@@ -531,6 +530,7 @@
                                         <td>
                                             <input type="text"
                                                    step="1"
+                                                   min="1"
                                                    id="imprestItemQty"
                                                    name="imprestItemQty"
                                                    class="form-control amount number_input"
