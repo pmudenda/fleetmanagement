ca76a04f62f0233f428d41baaa99034cd5d7852a
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-25 08:29:36 +0200

    shoq insurance state

--- resources/views/modules/userManagement/profileDelegation.blade.php
+++ resources/views/modules/userManagement/profileDelegation.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@php use Carbon\Carbon; @endphp
 @push('styles')
     <!-- DataTables -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
