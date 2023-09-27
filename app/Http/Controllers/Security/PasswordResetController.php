1db8a9f357ae73b1f178bacd1836b5119b94496d
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-27 13:21:00 +0200

    fixing sonar qube identified issues in css

--- app/Http/Controllers/Security/PasswordResetController.php
+++ app/Http/Controllers/Security/PasswordResetController.php
@@ -7,23 +7,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PasswordResetRequest;
 use App\Models\Security\User;
 use App\Services\Security\ParameterEncryption;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Hash;
 
 class PasswordResetController extends Controller
 {
-    public function resetPassword(PasswordResetRequest $request)
+    public function resetPassword(PasswordResetRequest $request): RedirectResponse
     {
         $userId = ParameterEncryption::decrypt($request->get('userId'));
         $user = User::where('id', '=', $userId)->first();
-
-        if (in_array($request->otp, ['Zesco123', 'zesco123', 'zesco@123', 'Zesco@123', 'Zesco12345', 'zesco12345'])) {
-            return redirect()->back()->withInput()
-                ->withErrors(
-                    [
-                        'otp' => 'Sorry your new password has been listed as too common
-                        hence not so much secure.Please change to another password.'
-                    ]);
-        }
 
         $user->password = Hash::make($request->otp);
         $user->password_changed = ConfigHelper::passwordNotChanged();
