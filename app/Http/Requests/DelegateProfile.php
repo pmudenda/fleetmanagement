938c02939527d393e70e7368c15b2e18d613f55f
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-27 14:50:57 +0200

    wrong column name

--- app/Http/Requests/DelegateProfile.php
+++ app/Http/Requests/DelegateProfile.php
@@ -28,7 +28,7 @@ class DelegateProfile extends FormRequest
             'staffNumber' => 'required|string|max:10|exists:SEC_USERS,staff_no',
             'employeeName' => 'required|string',
             'currentProfile' => 'required|numeric',
-            'startDate' => 'required|date_format:Y-m-d|after_or_equal:'.Carbon::now(),
+            'startDate' => 'required|date_format:Y-m-d|after_or_equal:'.Carbon::today(),
             'endDate' => 'required|date_format:Y-m-d|after:startDate',
             'remarks' => 'required|string|max:255|min:50',
         ];
