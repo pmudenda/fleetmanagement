c57cb8147a8ea94f8c58a0c627d72663e08bf593
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-30 16:35:33 +0200

    add approval hierarchy to workshop

--- app/View/Components/WorkshopWorkflowApprovers.php
+++ app/View/Components/WorkshopWorkflowApprovers.php
@@ -2,28 +2,69 @@
 
 namespace App\View\Components;
 
-use Illuminate\Contracts\View\Factory;
+use App\Models\Security\User;
+use App\Models\Workflow\WorkflowLog;
+use App\Models\Workflow\WorkflowTaskDetail;
 use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
 use Illuminate\View\Component;
 
 class WorkshopWorkflowApprovers extends Component
 {
+
     public $request;
+    public $task;
 
     /**
      * Create a new component instance.
      */
-    public function __construct($request)
+    public function __construct($request, $task)
     {
         $this->request = $request;
+        $this->task = $task;
     }
 
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Application|Factory
+    public function render(): View|string
     {
-        return view('components.workshop-workflow-approvers');
+        $documentStatus = $this->request->status;
+        $approvalsArray = WorkflowLog::where('reference', $this->task->reference)
+            ->orderBy('action_date')
+            ->get();
+
+        $steps = $approvalsArray->pluck('step_id')->toArray();
+        $currentStep = WorkflowTaskDetail::where('reference', '=', $this->task->reference)
+            ->first();
+
+        $claimant = User::where('staff_no', '=', $this->request->created_by)->first();
+        $supervisor = User::where('staff_no', '=', $claimant->supervisor_code)->first();
+        $manager = null;
+
+
+        if (!empty($supervisor) && ($currentStep->current_step_id == '03' || in_array('03', $steps))) {
+            $manager = User::where('staff_no', '=', $supervisor->supervisor_code)->first();
+        }
+
+        $snrManager = null;
+        $deputyDirector = null;
+        $director = null;
+        $managingDirector = null;
+
+        return view(
+            'components.workshop-workflow-approvers',
+            compact('documentStatus',
+                'steps',
+                'currentStep',
+                'approvalsArray',
+                'claimant',
+                'supervisor',
+                'manager',
+                'snrManager',
+                'deputyDirector',
+                'director',
+                'managingDirector'
+            )
+        );
     }
 }
