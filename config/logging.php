8fb7b6d5027f137d7399b3e4549fe31b6a4f5cdd
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-30 16:19:40 +0200

    update default host to default host ip

--- config/logging.php
+++ config/logging.php
@@ -4,7 +4,7 @@ use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
-const LOGS_LARAVEL_LOG = 'logs/laravel.log';
+
 return [
 
     /*
@@ -60,13 +60,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path(LOGS_LARAVEL_LOG),
+            'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path(LOGS_LARAVEL_LOG),
+            'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
@@ -117,7 +117,7 @@ return [
         ],
 
         'emergency' => [
-            'path' => storage_path(LOGS_LARAVEL_LOG),
+            'path' => storage_path('logs/laravel.log'),
         ],
     ],
 
