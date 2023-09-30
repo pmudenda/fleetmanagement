00daf571d57fc82f13e085cc4986c36d83f7c223
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-30 11:14:28 +0200

    vehicle reports

--- resources/views/modules/reports/index.blade.php
+++ resources/views/modules/reports/index.blade.php
@@ -310,18 +310,18 @@
                             ${accounting.formatMoney(params[0].data.value)}`;
                     }
                 },
-                dataset: {
+                /*dataset: {
                     dimensions: dataByYear.dimension,
                     source: dataByYear.source
-                },
-                /*legend: {
+                },*/
+                legend: {
                     type: 'scroll',
                     orient: 'horizontal',
                     right: 10,
                     top: 20,
                     bottom: 20,
                     data: dataByYear.years
-                },*/
+                },
                 xAxis: {
                     type: 'category',
                     data: dataByYear.years
@@ -366,94 +366,4 @@
 
         })(window.tmsApp || {});
     </script>
-
-    <script>
-        /* let chartDom = document.getElementById('pie2');
-         let myPieChart = echarts.init(chartDom);
-
-         let pieOption = {
-             legend: {},
-             tooltip: {
-                 trigger: 'axis',
-                 showContent: false
-             },
-             dataset: {
-                 source: [
-                     ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
-                     ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
-                     ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
-                     ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
-                     ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
-                 ]
-             },
-             xAxis: {type: 'category'},
-             yAxis: {gridIndex: 0},
-             grid: {top: '55%'},
-             series: [
-                 {
-                     type: 'line',
-                     smooth: true,
-                     seriesLayoutBy: 'row',
-                     emphasis: {focus: 'series'}
-                 },
-                 {
-                     type: 'line',
-                     smooth: true,
-                     seriesLayoutBy: 'row',
-                     emphasis: {focus: 'series'}
-                 },
-                 {
-                     type: 'line',
-                     smooth: true,
-                     seriesLayoutBy: 'row',
-                     emphasis: {focus: 'series'}
-                 },
-                 {
-                     type: 'line',
-                     smooth: true,
-                     seriesLayoutBy: 'row',
-                     emphasis: {focus: 'series'}
-                 },
-                 {
-                     type: 'pie',
-                     id: 'pie',
-                     radius: '30%',
-                     center: ['50%', '25%'],
-                     emphasis: {
-                         focus: 'self'
-                     },
-                     label: {
-                         formatter: '{b}: {@2012} ({d}%)'
-                     },
-                     encode: {
-                         itemName: 'product',
-                         value: '2012',
-                         tooltip: '2012'
-                     }
-                 }
-             ]
-         };
-         myPieChart.on('updateAxisPointer', function (event) {
-             const xAxisInfo = event.axesInfo[0];
-             if (xAxisInfo) {
-                 const dimension = xAxisInfo.value + 1;
-                 myChart.setOption({
-                     series: {
-                         id: 'pie',
-                         label: {
-                             formatter: '{b}: {@[' + dimension + ']} ({d}%)'
-                         },
-                         encode: {
-                             value: dimension,
-                             tooltip: dimension
-                         }
-                     }
-                 });
-             }
-         });
-
-         myPieChart &&
-         myPieChart.setOption(pieOption);*/
-    </script>
-
 @endpush
