ff6a950f756ad7f55e6f212a1240d5099628aae4
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-09-30 16:26:39 +0200

    update default host to default host ip

--- routes/workshop.php
+++ routes/workshop.php
@@ -48,7 +48,7 @@ Route::group(['middleware' => ['auth', 'is.active', 'change.password'],
 
         // front desk
         Route::get('vehicle/workshop/checkin', [MaintenanceController::class, 'start'])
-            ->name('vehicle.workshop.checkin');
+            ->name('workshop.checkin');
 
         Route::post('vehicle/workshop/create-task', [MaintenanceController::class, 'createTaskForWorkShopSupervisor'])
             ->name('vehicle.workshop.checkin');
