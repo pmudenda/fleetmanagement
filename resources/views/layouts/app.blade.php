2dc6c9e7d749af2e9863e7325b9a463d0b8a9863
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2023-10-01 17:44:38 +0200

    Add livewire added many - many workshop

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,38 +8,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:locale" content="en_US">
     <link rel="canonical" href="">
-    <link rel="shortcut icon" href="{{ secure_asset('assets/dist/img/icons/logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/dist/img/icons/logo.png') }}" type="image/x-icon">
 
-    <link rel="stylesheet" href="{{secure_asset('themes/plugins/fontawesome-free/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{secure_asset('themes/ionicons/2.0.1/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('themes/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('themes/ionicons/2.0.1/css/ionicons.min.css')}}">
 
     {{--<link rel="stylesheet"
           type="text/css"
-          href="{{secure_asset('themes/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">--}}
-    <link rel="stylesheet" href="{{secure_asset('themes/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ secure_asset('libs/bootstrap-5.2.3/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('themes/dist/css/adminlte.min2167.css')}}?v=3.2.0">
+          href="{{asset('themes/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">--}}
+    <link rel="stylesheet" href="{{asset('themes/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('libs/bootstrap-5.2.3/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('themes/dist/css/adminlte.min2167.css')}}?v=3.2.0">
     <link rel="stylesheet" type="text/css"
-          href="{{secure_asset('themes/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('themes/plugins/daterangepicker/daterangepicker.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('themes/plugins/summernote/summernote-bs4.min.css')}}">
+          href="{{asset('themes/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('themes/plugins/daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('themes/plugins/summernote/summernote-bs4.min.css')}}">
 
     <!--Add Ons-->
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/frappe/css/desk.bundle.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/css/style.bundle.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/frappe/css/desk.bundle.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.bundle.css') }}">
 
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/plugins/toastr/toastr.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/plugins/sweetalert2/sweetalert2.min.css')}}"/>
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/plugins/vue-select/vue-select.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/custom/datatables/datatables.bundle.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset("assets/plugins/jquery.filer/css/jquery.filer.css")}}"/>
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/css/custom.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/plugins/toastr/toastr.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/plugins/sweetalert2/sweetalert2.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/plugins/vue-select/vue-select.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/custom/datatables/datatables.bundle.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset("assets/plugins/jquery.filer/css/jquery.filer.css")}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/custom.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/plugins/select2/css/select2.min.css')}}">
     <link rel="stylesheet" type="text/css"
-          href="{{secure_asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{secure_asset('libs/session.timeout/session.timeout.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/css/asyncLoader.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/css/jquery-ui.css')}}"/>
+          href="{{asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('libs/session.timeout/session.timeout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/asyncLoader.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/jquery-ui.css')}}"/>
     <livewire:styles />
 
     <style>
@@ -52,9 +52,9 @@
         }
     </style>
     <link rel="stylesheet" type="text/css"
-          href="{{secure_asset('assets/plugins/datatables/datetime/css/dataTables.dateTime.min.css')}}"/>
+          href="{{asset('assets/plugins/datatables/datetime/css/dataTables.dateTime.min.css')}}"/>
     <link rel="stylesheet" type="text/css"
-          href="{{secure_asset('assets/plugins/datatables/searchbuilder/css/searchBuilder.dataTables.min.css')}}"/>
+          href="{{asset('assets/plugins/datatables/searchbuilder/css/searchBuilder.dataTables.min.css')}}"/>
     @stack('styles')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed sidebar-collapse">
@@ -490,31 +490,31 @@
 <input type="hidden" name="currentUser" id="currentUser" value="{{Auth::user()->staff_no}}"/>
 @include('modules.vehicleManagement.partial.data_end_point')
 <audio preload="auto" id="sound-email" volume=0.1>
-    <source src="{{secure_asset('assets/sounds/email.mp3')}}"/>
+    <source src="{{asset('assets/sounds/email.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-submit" volume=0.1>
-    <source src="{{secure_asset('assets/sounds/submit.mp3')}}"/>
+    <source src="{{asset('assets/sounds/submit.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-cancel" volume=0.1>
-    <source src="{{secure_asset('assets/sounds/cancel.mp3')}}"/>
+    <source src="{{asset('assets/sounds/cancel.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-delete" volume=0.05>
-    <source src="{{secure_asset('assets/sounds/delete.mp3')}}"/>
+    <source src="{{asset('assets/sounds/delete.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-click" volume=0.05>
-    <source src="{{secure_asset('assets/sounds/click.mp3')}}"/>
+    <source src="{{asset('assets/sounds/click.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-error" volume=0.1>
-    <source src="{{secure_asset('assets/sounds/error.mp3')}}"/>
+    <source src="{{asset('assets/sounds/error.mp3')}}"/>
 </audio>
 
 <audio preload="auto" id="sound-alert" volume=0.2>
-    <source src="{{secure_asset('assets/sounds/alert.mp3')}}"/>
+    <source src="{{asset('assets/sounds/alert.mp3')}}"/>
 </audio>
 
 <script>
@@ -528,15 +528,15 @@
     tmsApp.csrf_token = document.querySelector('meta[name="csrf-token"]').content;
 </script>
 
-<script src="{{secure_asset('assets/plugins/jquery/jquery-3.6.3.js')}}"></script>
-<script src="{{secure_asset('themes/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-<script src="{{secure_asset('libs/echarts@5.4.2/dist/echarts.min.js')}}"></script>
+<script src="{{asset('assets/plugins/jquery/jquery-3.6.3.js')}}"></script>
+<script src="{{asset('themes/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('libs/echarts@5.4.2/dist/echarts.min.js')}}"></script>
 <script>
     $.widget.bridge('uibutton', $.ui.button)
 </script>
-<script src="{{secure_asset('themes/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{secure_asset('libs/bootstrap-5.2.3/js/bootstrap.bundle.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/toastr/toastr.min.js')}}"></script>
+<script src="{{asset('themes/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('libs/bootstrap-5.2.3/js/bootstrap.bundle.js')}}"></script>
+<script src="{{asset('assets/plugins/toastr/toastr.min.js')}}"></script>
 <script>
     $(document).ready(function () {
         toastr.options = {
@@ -554,30 +554,30 @@
         tmsApp.toast = new bootstrap.Toast(toastLiveExample)
     })
 </script>
-<script src="{{secure_asset('assets/plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/jquery.filer/js/jquery.filer.min.js')}}"></script>
-<script src="{{secure_asset('libs/jqueryInputmask/3.3.4/jquery.inputmask.bundle.min.js') }}"></script>
-<script src="{{secure_asset('assets/plugins/form-masking/autoNumeric.js') }}"></script>
-<script src="{{secure_asset('themes/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-<script src="{{secure_asset('themes/plugins/moment/moment.min.js')}}"></script>
-<script src="{{secure_asset('themes/plugins/daterangepicker/daterangepicker.js')}}"></script>
-<script src="{{secure_asset('themes/plugins/summernote/summernote-bs4.min.js')}}"></script>
-<script src="{{secure_asset('themes/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-<script src="{{secure_asset('assets/js/accounting.min.js') }}"></script>
-<script src="{{secure_asset('assets/plugins/select2/js/select2.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/vue/vue.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/vue-select/vue-select.js')}}"></script>
-<script src="{{secure_asset('assets/plugins/vue-select2/js/vue-select.js')}}"></script>
-<script src="{{secure_asset('themes/dist/js/adminlte2167.js')}}?v=3.2.0"></script>
-<script src="{{secure_asset('assets/js/system/core.js')}}"></script>
-<script src="{{secure_asset('assets/js/system/workflow_approvals.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{secure_asset('assets/js/global/async.loader.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{secure_asset('assets/js/global/page.loader.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{secure_asset('assets/js/global/system_alerts.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{secure_asset('assets/js/global/custom_filer.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{ secure_asset('libs/session.timeout/session.timeout.js').'?v='.Carbon::now()->format('his')}}"></script>
-<script src="{{ secure_asset('libs/qrcode/qrcode.min.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{asset('assets/plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
+<script src="{{asset('assets/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+<script src="{{asset('assets/plugins/jquery.filer/js/jquery.filer.min.js')}}"></script>
+<script src="{{asset('libs/jqueryInputmask/3.3.4/jquery.inputmask.bundle.min.js') }}"></script>
+<script src="{{asset('assets/plugins/form-masking/autoNumeric.js') }}"></script>
+<script src="{{asset('themes/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{asset('themes/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('themes/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('themes/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{asset('themes/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{asset('assets/js/accounting.min.js') }}"></script>
+<script src="{{asset('assets/plugins/select2/js/select2.js')}}"></script>
+<script src="{{asset('assets/plugins/vue/vue.js')}}"></script>
+<script src="{{asset('assets/plugins/vue-select/vue-select.js')}}"></script>
+<script src="{{asset('assets/plugins/vue-select2/js/vue-select.js')}}"></script>
+<script src="{{asset('themes/dist/js/adminlte2167.js')}}?v=3.2.0"></script>
+<script src="{{asset('assets/js/system/core.js')}}"></script>
+<script src="{{asset('assets/js/system/workflow_approvals.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{asset('assets/js/global/async.loader.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{asset('assets/js/global/page.loader.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{asset('assets/js/global/system_alerts.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{asset('assets/js/global/custom_filer.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{ asset('libs/session.timeout/session.timeout.js').'?v='.Carbon::now()->format('his')}}"></script>
+<script src="{{ asset('libs/qrcode/qrcode.min.js').'?v='.Carbon::now()->format('his')}}"></script>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <livewire:scripts />
 
