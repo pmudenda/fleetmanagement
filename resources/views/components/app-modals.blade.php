8371c018650de2b40a32c6cf3af881fe8c6485c2
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-10-01 15:22:37 +0200

    cancellation delegation

--- resources/views/components/app-modals.blade.php
+++ resources/views/components/app-modals.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon;use Illuminate\Support\Facades\Auth; @endphp
 <div>
     <div aria-live="polite" aria-atomic="true" class="position-relative">
         <div class="toast-container position-fixed top-0 end-0 p-3">
