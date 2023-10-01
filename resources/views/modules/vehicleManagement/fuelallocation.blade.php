ea1bbbb2cda8a586d17d56f7373da2c3ce3e3bec
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-10-01 22:33:31 +0200

    profile creation

--- resources/views/modules/vehicleManagement/fuelallocation.blade.php
+++ resources/views/modules/vehicleManagement/fuelallocation.blade.php
@@ -56,7 +56,7 @@
                             <div class="container-fluid mt-2">
                                 <div class="row">
                                     <div class="col-9">
-                                        <div class="row">
+                                        <div class="row pl-0">
                                             <div class="col-9">
                                                 <div class="col-xs-12 col-sm-6 col-md-6">
                                                     <div class="container-fluid pl-0">
