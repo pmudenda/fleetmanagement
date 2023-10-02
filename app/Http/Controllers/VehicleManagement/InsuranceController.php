2bb03c22bd9152d184769e8af155b3e891269c06
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-10-02 11:12:25 +0200

    user simulation

---
+++ app/Http/Controllers/VehicleManagement/InsuranceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\VehicleManagement;
+
+use App\Http\Controllers\Controller;
+
+class InsuranceController extends Controller
+{
+    public function create()
+    {
+
+    }
+}
