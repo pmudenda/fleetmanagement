16e07783cae0f123cf546be4b58b521effea3dac
harrymackinson@hotmail.com <harrymackinson@hotmail.com>
2023-10-02 08:55:10 +0200

    profile creation

---
+++ app/Http/Controllers/TaskManagementController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Workflow\WorkflowService;
+use Illuminate\Contracts\View\View;
+
+class TaskManagementController extends Controller
+{
+    private WorkflowService $workflowService;
+
+    public function __construct(WorkflowService $workflowService)
+    {
+        $this->workflowService = $workflowService;
+    }
+
+    public function view(): View
+    {
+        $approvalTasks = $this->workflowService->getAllWorkflowTasks();
+
+        return view('modules.workflow.tasks')
+            ->with(compact('approvalTasks'));
+    }
+}
