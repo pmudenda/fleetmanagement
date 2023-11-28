c4d7c08931bce51859332f4832967169ce93f07d
Vunga Singogo (SNR ENG-IVsingogoSD) (ICT) <vsingogo@zesco.co.zm>
2023-11-28 23:00:42 +0200

    preview selected Image

--- public/modules/vehicleManagement/assets/js/new-vehicle-registration.js
+++ public/modules/vehicleManagement/assets/js/new-vehicle-registration.js
@@ -2294,10 +2294,39 @@ function checkOnboardingHeaderStatus() {
         $(fileInput).trigger('click');
     });
 
+    function previewVehicleImage(event) {
+        //$('#frame').src = URL.createObjectURL(event.target.files[0]);
+        let uploadFile = $(event.target);
+        let self = event.target;
+        let files = !!self.files ? self.files : [];
+        if (!files.length || !window.FileReader) return;
+        // no file selected, or no FileReader support
+
+        if (/^image/.test(files[0].type)) {
+            // only image file
+            let reader = new FileReader();
+            // instance of the FileReader
+            reader.readAsDataURL(files[0]);
+            // read the local file
+
+            reader.onloadend = function () {
+                // set image data as background of div
+                uploadFile.closest("div").find('.imagePreview').css({
+                    "background-image": "url(" + this.result + ")", 'display': 'block'
+                });
+            }
+
+            $(uploadFile).closest('div').find('p').addClass('d-none');
+        } else {
+
+            toastr.error('only image (.jpg, .jpeg, .png, .bmp) file types are allowed', 'Invalid File Format Selected')
+        }
+    }
+
     let fileSelects = [].slice.call(document.querySelectorAll('.fileElem'));
     fileSelects.map(function (fileSelect) {
         fileSelect.addEventListener("change", (e) => {
-            app.preview(e);
+            previewVehicleImage(e);
         }, false);
     });
 
