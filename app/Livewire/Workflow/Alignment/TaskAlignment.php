8dc040814a754722cbeda2a80dcbb31909599113
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2024-01-31 18:45:17 +0200

    Add livewire added many - many workshop

--- app/Livewire/Workflow/Alignment/TaskAlignment.php
+++ app/Livewire/Workflow/Alignment/TaskAlignment.php
@@ -8,20 +8,24 @@ use App\Models\Workflow\WorkflowTaskHeader;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
-class TaskAlignment extends Component {
+class TaskAlignment extends Component
+{
     public $staff_number_from, $staff_number_to;
-
+    public $userFrom, $userTo;
 
     public $selected_tasks = [];
 
-    public function rules() {
+    public function rules()
+    {
         return [
-            'staff_number_to' => 'required',
+            'staff_number_from' => 'required|exists:sec_users,staff_no',
+            'staff_number_to' => 'required|exists:sec_users,staff_no',
             'selected_tasks.*' => ['required']
         ];
     }
 
-    public function render() {
+    public function render()
+    {
         $tasks = [];
 
         $users = Cache::remember('users', 60 * 60 * 5, function () {
@@ -36,23 +40,40 @@ class TaskAlignment extends Component {
         return view('livewire.workflow.alignment.task-alignment', compact('tasks', 'users'));
     }
 
-    public function search() {
-
+    public function search_from()
+    {
+        $this->validateOnly('staff_number_from');
+        $this->userFrom = User::where('staff_no', $this->staff_number_from)->first();
     }
 
-    public function assign() {
+    public function search_to()
+    {
+        $this->validateOnly('staff_number_to');
+        $this->userTo = User::where('staff_no', $this->staff_number_to)->first();
+    }
+
+    public function assign()
+    {
         $this->validate();
         $updated = WorkflowTaskHeader::whereIn('reference', $this->selected_tasks)
             ->where('ASSIGNED_USER', $this->staff_number_from)
             ->update(['ASSIGNED_USER' => $this->staff_number_to]);
-        if($updated){
-             WorkflowTaskDetail::whereIn('reference', $this->selected_tasks)
+        if ($updated) {
+            WorkflowTaskDetail::whereIn('reference', $this->selected_tasks)
                 ->where('ACTIONING_OFFICER', $this->staff_number_from)
                 ->update(['ACTIONING_OFFICER' => $this->staff_number_to]);
 
-             $this->reset('staff_number_from','staff_number_to','selected_tasks');
+            $this->reset('staff_number_from', 'staff_number_to', 'selected_tasks');
             $this->dispatch('message', 'User tasks have been aligned successfully');
 
         }
+    }
+
+    public function selectAll()
+    {
+        $tasks = WorkflowTaskHeader::where('ASSIGNED_USER', $this->staff_number_from)
+            ->whereNull('DATE_ENDED')
+            ->get();
+        $this->selected_tasks = $tasks->pluck('reference')->values()->all();
     }
 }
--- resources/views/livewire/workflow/alignment/task-alignment.blade.php
+++ resources/views/livewire/workflow/alignment/task-alignment.blade.php
@@ -8,19 +8,134 @@
 
         <div class="row">
             <div class="col-lg-4">
-                <div class="card ">
-                    <div class="card-header">
-                        <div class="card-title">
-                            Employee Search
+                <div class="col-lg-12">
+                    <div class="card ">
+                        <div class="card-header">
+                            <div class="card-title">
+                                Move Tasks From:
+                            </div>
+                            <div class="card-toolbar justify-content-end">
+
+                            </div>
                         </div>
-                        <div class="card-toolbar justify-content-end">
+
+                        <div class="card-body">
+
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+
+                                        <div class="input-group" wire:ignore>
+                                            <input id="staff_number_from" class="form-control"
+                                                   wire:model="staff_number_from" placeholder="Staff Number"/>
+                                            <div class="input-group-append">
+                                                <x-button type="button" class="btn btn-primary btn-sm"
+                                                          wire:target="search_from"
+                                                          wire:click="search_from">Search
+                                                </x-button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+
+                                </div>
+                            </div>
+
+                            <dl class="row">
+                                <dt class="col-lg-3">Staff No</dt>
+                                <dd class="col-lg-9 text-right">{{$userFrom->staff_no ?? '--'}}</dd>
+
+                                <dt class="col-lg-3">Name</dt>
+                                <dd class="col-lg-9 text-right">{{$userFrom->name ?? '--'}}</dd>
+
+                                <dt class="col-lg-3">Job Title</dt>
+                                <dd class="col-lg-9 text-right">{{$userFrom->job_title ?? '--'}}</dd>
+                            </dl>
 
                         </div>
+
+
                     </div>
+                </div>
 
-                    <div class="card-body">
-                        <div class="row">
+                <div class="col-lg-12">
+                    <div class="card ">
+                        <div class="card-header">
+                            <div class="card-title">
+                                Move Tasks To:
+                            </div>
+                            <div class="card-toolbar justify-content-end">
 
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <div class="input-group" wire:ignore>
+                                            <input id="staff_number_to" class="form-control"
+                                                   wire:model="staff_number_to" placeholder="Staff No"/>
+                                            <div class="input-group-append">
+                                                <x-button type="button" class="btn btn-primary btn-sm"
+                                                          wire:target="search_to"
+                                                          wire:click="search_to">Search
+                                                </x-button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <dl class="row">
+                                <dt class="col-lg-3">Staff No</dt>
+                                <dd class="col-lg-9 text-right">{{$userTo->staff_no ?? '--'}}</dd>
+
+                                <dt class="col-lg-3">Name</dt>
+                                <dd class="col-lg-9 text-right">{{$userTo->name ?? '--'}}</dd>
+
+                                <dt class="col-lg-3">Job Title</dt>
+                                <dd class="col-lg-9 text-right">{{$userTo->job_title ?? '--'}}</dd>
+                            </dl>
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-8">
+                <form wire:submit.prevent="assign">
+                    <div class="card ">
+                        <div class="card-header">
+                            <div class="card-title">
+                                Tasks
+                            </div>
+                            <div class="card-toolbar justify-content-end">
+                                @if(!empty($tasks))
+                                    <form class="form-inline">
+                                        <x-button type="button" class="btn btn-light-primary btn-sm ml-3"
+                                                  wire:target="selectAll" wire:click="selectAll">
+                                            Select All
+                                        </x-button>
+
+                                        <x-button type="submit" class="btn btn-primary btn-sm  ml-3" wire:target="assign">
+                                            Assign
+                                        </x-button>
+
+
+                                    </form>
+                                @endif
+
+                            </div>
+                        </div>
+
+                        <div class="card-body">
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <ul>
@@ -31,85 +146,36 @@
                                 </div>
                             @endif
 
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group" wire:ignore>
-                                        <label>Staff Number</label>
-                                        <input id="staff_number_from" class="form-control" wire:model="staff_number_from">
+                            <table class="table table-bordered">
+                                <thead>
+                                <th></th>
+                                <th>Reference</th>
+                                <th>Subject</th>
+                                <th>Priority</th>
+                                <th>Created_at</th>
+                                </thead>
 
-                                    </div>
-                                </div>
-                            </div>
-
-                            <x-button type="button" class="btn btn-primary btn-sm" wire:target="search"
-                                      wire:click="search">Search
-                            </x-button>
-
+                                <tbody>
+                                @foreach($tasks as $task)
+                                    <tr>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox"
+                                                       value="{{$task->reference}}" wire:model="selected_tasks"/>
+                                            </div>
+                                        </td>
+                                        <td>{{$task->reference}}</td>
+                                        <td>{{$task->subject}}</td>
+                                        <td>{{$task->priority}}</td>
+                                        <td>{{$task->created_at->toFormattedDateString()}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-
-
-                </div>
-            </div>
-
-            <div class="col-lg-8">
-              <form wire:submit.prevent="assign">
-                  <div class="card ">
-                      <div class="card-header">
-                          <div class="card-title">
-                              Tasks
-                          </div>
-                          <div class="card-toolbar justify-content-end">
-                              <form class="form-inline">
-                                  <input type="text" class="form-control w-50 " id="inlineFormInputName2" placeholder="Assign to" wire:model="staff_number_to">
-
-                                  <x-button type="submit" class="btn btn-primary btn-sm ml-3" wire:target="assign">Assign</x-button>
-                              </form>
-                          </div>
-                      </div>
-
-                      <div class="card-body">
-                          <table class="table table-bordered">
-                              <thead>
-                              <th></th>
-                              <th>Reference</th>
-                              <th>Subject</th>
-                              <th>Priority</th>
-                              <th>Created_at</th>
-                              </thead>
-
-                              <tbody>
-                              @forelse($tasks as $task)
-                                  <tr>
-                                      <td>
-                                          <div class="form-check">
-                                              <input class="form-check-input" type="checkbox" value="{{$task->reference}}" wire:model="selected_tasks">
-                                          </div>
-                                      </td>
-                                      <td>{{$task->reference}}</td>
-                                      <td>{{$task->subject}}</td>
-                                      <td>{{$task->priority}}</td>
-                                      <td>{{$task->created_at->toFormattedDateString()}}</td>
-                                  </tr>
-                              @empty
-
-                              @endforelse
-                              </tbody>
-                          </table>
-                      </div>
-                  </div>
-              </form>
+                </form>
             </div>
         </div>
     </div>
 </section>
-@push('scripts')
-    <script type="text/javascript">
-        $(document).ready(function (){
-            $('#staff_number_from').on('select2:select', function (e) {
-                var data = e.params.data;
-                $wire.staff_number_from = data.id;
-            });
-        });
-    </script>
-@endpush
