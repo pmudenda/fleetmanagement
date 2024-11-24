f3211538116134299be004ce77691a776952c404
jamestembo <jamestembo@zesco.co.zm>
2024-11-24 10:21:49 +0200

    verifying

--- resources/views/layouts/menus/side_bar.blade.php
+++ resources/views/layouts/menus/side_bar.blade.php
@@ -113,17 +113,17 @@
                                 </li>
                             @endcan
 
-                                @can(config('rights.town_add'))
+                            @can(config('rights.town_add'))
 
-                                    <li class="nav-item pl-2">
-                                        <a href="{{ route('general.town.index')}}"
-                                           class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Towns</p>
-                                        </a>
-                                    </li>
+                                <li class="nav-item pl-2">
+                                    <a href="{{ route('general.town.index')}}"
+                                       class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Towns</p>
+                                    </a>
+                                </li>
 
-                                @endcanany
+                            @endcanany
 
                             <li class="nav-item d-none">
                                 <a href="#" class="nav-link">
@@ -239,12 +239,12 @@
                                 <svg width="24" height="24" viewBox="0 0 24 24"
                                      fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
-                                        d="M20 14H18V10H20C20.6 10 21 10.4
+                                            d="M20 14H18V10H20C20.6 10 21 10.4
                                             21 11V13C21 13.6 20.6 14 20 14ZM21 19V17C21
                                             16.4 20.6 16 20 16H18V20H20C20.6 20 21 19.6 21
                                             19ZM21 7V5C21 4.4 20.6 4 20 4H18V8H20C20.6 8
                                             21 7.6 21 7Z"
-                                        fill="currentColor"></path>
+                                            fill="currentColor"></path>
                                     <path opacity="0.3"
                                           d="M17 22H3C2.4 22 2 21.6 2 21V3C2 2.4 2.4 2 3
                                           2H17C17.6 2 18 2.4 18 3V21C18 21.6 17.6 22 17
@@ -346,7 +346,13 @@
                                 <li class="nav-item pl-2">
                                     <a class="nav-link" href="{{URL::signedRoute('list.workshop.requisition') }}">
                                         <i class="fas fa-list nav-icon"></i>
-                                        <p>Requisitions</p>
+                                        <p>Requisitions Listing</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item pl-2">
+                                    <a class="nav-link" href="{{URL::signedRoute('reject.workshop.requisition') }}">
+                                        <i class="fas fa-list nav-icon"></i>
+                                        <p>Requisitions Management</p>
                                     </a>
                                 </li>
                             @endcanany
@@ -398,14 +404,14 @@
                                             </li>
                                         @endcanany
 
-{{--                                            @canany([config('rights.add_user')])--}}
-{{--                                                <li class="nav-item">--}}
-{{--                                                    <a class="nav-link" href="{{route('users.new.external')}}">--}}
-{{--                                                        <i class="fas fa-user-plus nav-icon"></i>--}}
-{{--                                                        <p class="menu-title">Add External </p>--}}
-{{--                                                    </a>--}}
-{{--                                                </li>--}}
-{{--                                            @endcanany--}}
+                                        {{--                                            @canany([config('rights.add_user')])--}}
+                                        {{--                                                <li class="nav-item">--}}
+                                        {{--                                                    <a class="nav-link" href="{{route('users.new.external')}}">--}}
+                                        {{--                                                        <i class="fas fa-user-plus nav-icon"></i>--}}
+                                        {{--                                                        <p class="menu-title">Add External </p>--}}
+                                        {{--                                                    </a>--}}
+                                        {{--                                                </li>--}}
+                                        {{--                                            @endcanany--}}
 
                                         @canany([config('rights.view_user')])
                                             <li class="nav-item">
@@ -416,14 +422,14 @@
                                             </li>
                                         @endcanany
 
-                                            @can([config('rights.user_realign')])
-                                                <li class="nav-item">
-                                                    <a class="nav-link" href="{{route('users.realign')}}">
-                                                        <i class="fas fa-user-plus nav-icon"></i>
-                                                        <p class="menu-title">Re-Align</p>
-                                                    </a>
-                                                </li>
-                                            @endcan
+                                        @can([config('rights.user_realign')])
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{route('users.realign')}}">
+                                                    <i class="fas fa-user-plus nav-icon"></i>
+                                                    <p class="menu-title">Re-Align</p>
+                                                </a>
+                                            </li>
+                                        @endcan
                                     </ul>
                                 </li>
                             @endcanany
@@ -608,13 +614,13 @@
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path
-                                        d="M17.5 11H6.5C4 11 2 9 2
+                                            d="M17.5 11H6.5C4 11 2 9 2
                                             6.5C2 4 4 2 6.5 2H17.5C20 2 22 4
                                             22 6.5C22 9 20 11 17.5 11ZM15
                                             6.5C15 7.9 16.1 9 17.5 9C18.9 9
                                             20 7.9 20 6.5C20 5.1 18.9
                                             4 17.5 4C16.1 4 15 5.1 15 6.5Z"
-                                        fill="currentColor"></path>
+                                            fill="currentColor"></path>
                                     <path opacity="0.3"
                                           d="M17.5 22H6.5C4 22 2 20 2 17.5C2 15
                                           4 13 6.5 13H17.5C20 13 22 15 22 17.5C22
@@ -953,8 +959,6 @@
                                         @endcanany
 
 
-
-
                                     </ul>
                                 </li>
                             @endcanany
@@ -979,7 +983,7 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                                @can(config('rights.tasks_realign'))
+                            @can(config('rights.tasks_realign'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('tasks.realign') }}">
                                         <i class="far fa-circle nav-icon"></i>
@@ -1130,15 +1134,15 @@
                         <ul class="nav nav-treeview pl-3">
                             @canany(config('rights.access_reports'),
                                                     config('rights.access_fuel_reports'))
-{{--                                <li class="nav-item">--}}
-{{--                                    <a href="{{URL::signedRoute('reports.fuel.requisitions')}}"--}}
-{{--                                       class="nav-link">--}}
-{{--                                        <i class="far fa-circle nav-icon"></i>--}}
-{{--                                        <p>--}}
-{{--                                            Fuel--}}
-{{--                                        </p>--}}
-{{--                                    </a>--}}
-{{--                                </li>--}}
+                                {{--                                <li class="nav-item">--}}
+                                {{--                                    <a href="{{URL::signedRoute('reports.fuel.requisitions')}}"--}}
+                                {{--                                       class="nav-link">--}}
+                                {{--                                        <i class="far fa-circle nav-icon"></i>--}}
+                                {{--                                        <p>--}}
+                                {{--                                            Fuel--}}
+                                {{--                                        </p>--}}
+                                {{--                                    </a>--}}
+                                {{--                                </li>--}}
 
                                 <li class="nav-item">
                                     <a href="{{route('reports.fuel.status')}}"
@@ -1165,25 +1169,25 @@
                                        class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>
-                                           Consolidated Spares Report
+                                            Consolidated Spares Report
                                         </p>
                                     </a>
                                 </li>
                             @endcanany
 
-{{--                            @canany(config('rights.access_reports'),--}}
-{{--                                config('rights.access_vehicle_status_reports'))--}}
-{{--                                <li class="nav-item">--}}
-{{--                                    <a href="{{--}}
-{{--                                        URL::signedRoute('reports.vehicle.status',--}}
-{{--                                        ['step'=> 1]--}}
-{{--                                    )}}"--}}
-{{--                                       class="nav-link">--}}
-{{--                                        <i class="far fa-circle nav-icon"></i>--}}
-{{--                                        <p>Vehicles</p>--}}
-{{--                                    </a>--}}
-{{--                                </li>--}}
-{{--                            @endcanany--}}
+                            {{--                            @canany(config('rights.access_reports'),--}}
+                            {{--                                config('rights.access_vehicle_status_reports'))--}}
+                            {{--                                <li class="nav-item">--}}
+                            {{--                                    <a href="{{--}}
+                            {{--                                        URL::signedRoute('reports.vehicle.status',--}}
+                            {{--                                        ['step'=> 1]--}}
+                            {{--                                    )}}"--}}
+                            {{--                                       class="nav-link">--}}
+                            {{--                                        <i class="far fa-circle nav-icon"></i>--}}
+                            {{--                                        <p>Vehicles</p>--}}
+                            {{--                                    </a>--}}
+                            {{--                                </li>--}}
+                            {{--                            @endcanany--}}
 
                         </ul>
                     </li>
