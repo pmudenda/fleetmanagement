ecc979cd19c2591afb0a357241940d15221ff622
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2024-11-25 12:32:18 +0200

    Updated out of town message

--- app/Livewire/Reports/Sprares/SparesPeriodReport.php
+++ app/Livewire/Reports/Sprares/SparesPeriodReport.php
@@ -31,16 +31,17 @@ class SparesPeriodReport extends Component
         array_unshift($columns,'#');
 
         $rows = DB::table('MERGED_SPARES_REPORT_VIEW')
-            ->where('DOCUMENT_DATE','>=',$this->from )
-            ->where('DOCUMENT_DATE','<=',  $this->to);
+            ->where('UPDATED_AT','>=',$this->from )
+            ->where('UPDATED_AT','<=',  $this->to);
 
         $total_amount = $rows->sum('VALUE_AMOUNT');
-        $rows = $rows->paginate(10);
+        $rows = $rows->paginate(25);
 
         return view('livewire.reports.sprares.spares-period-report',compact('rows','columns','total_amount'));
     }
 
     public function search(){
+        $this->resetPage();
         $this->validate();
     }
 
@@ -49,9 +50,9 @@ class SparesPeriodReport extends Component
         $columns = Schema::getColumnListing('MERGED_SPARES_REPORT_VIEW');
         array_unshift($columns,'#');
         $rows = DB::table('MERGED_SPARES_REPORT_VIEW')
-            ->where('month','>=',Carbon::createFromFormat('Y-m-d', $this->from)->format('Ym') )
-            ->where('month','<=',  Carbon::createFromFormat('Y-m-d',$this->to)->format('Ym'))
-            ->orderBy('month');
+            ->where('UPDATED_AT','>=',$this->from )
+            ->where('UPDATED_AT','<=',  $this->to)
+            ->orderBy('UPDATED_AT');
 
         ini_set('memory_limit','-1');
         return (new ConsolidateSparesReportExport($rows,$columns))->download('ConsolidatedSparesReport.xlsx');
