950b13c1f926735d40fe924c4c416dcaf58a09c6
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2025-06-12 09:39:10 +0200

    Repaired Images

--- resources/views/modules/userManagement/index.blade.php
+++ resources/views/modules/userManagement/index.blade.php
@@ -174,6 +174,11 @@
                                 </table>
                             </div>
                         </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-center">
+                                {{ $users->links() }}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
