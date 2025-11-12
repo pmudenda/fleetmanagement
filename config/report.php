41d438a988c062d352e89a8e8eeb34f144c77279
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2025-11-12 11:08:25 +0200

    changed fuel requisition

--- config/report.php
+++ config/report.php
@@ -5,8 +5,8 @@ return [
         'query' => "RPT_PURCHASE_ORDER_CHECK_MVIEW"
     ],
 
-    'RPT_PURCHASE_REQUEST_CHECK_VIEW' => [
+    'RPT_PURCHASE_REQUISITION_CHECK_VIEW' => [
     'title' => "Purchase Requisition Check",
-    'query' => "RPT_PURCHASE_REQUEST_CHECK_VIEW"
+    'query' => "RPT_PURCHASE_REQUISITION_CHECK_VIEW"
 ]
 ];
--- resources/views/livewire/report/report-index.blade.php
+++ resources/views/livewire/report/report-index.blade.php
@@ -1,21 +1,22 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Reports</h3>
-            </div>
+<section class="content">
+    <x-error-view/>
+    <x-content-header :pageTitle="'Reports'"
+                      :activeCrumb="'Reports'"
+                      :link="'report.index'"
+                      :linkText="'Reports'"/>
 
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-12">
-                        @foreach($reports as $id => $report)
-                            <a href="{{route('report.view', $id)}}">{{$report['title']}}</a>
-                        @endforeach
+<div class="container-fluid">
+    <div class="row">
+        @foreach($reports as $id => $report)
+            <div class="col-12 col-sm-4">
+                <div class="info-box bg-light">
+                    <div class="info-box-content">
+                        <span class="info-box-text text-center text-primary">{{$report['title']}}</span>
+                        <a href="{{route('report.view', $id)}}" class="text-center">View</a>
                     </div>
                 </div>
             </div>
-
-        </div>
-
+        @endforeach
     </div>
 </div>
+</section>
