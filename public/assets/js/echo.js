91e01b26c7002e1b84da20f4260777cc147651c0
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2026-01-25 14:11:16 +0200

    changed fuel requisition

--- public/assets/js/echo.js
+++ public/assets/js/echo.js
@@ -23654,7 +23654,7 @@ window.Pusher = (pusher_js__WEBPACK_IMPORTED_MODULE_1___default());
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'reverb',
   key: "pvh9bbosndvrmimrlypc",
-  wsHost: "10.16.30",
+  wsHost: "10.16.1.30",
   wsPort: "9000",
   wssPort: "9000",
   forceTLS: ((_process$env$MIX_REVE = "http") !== null && _process$env$MIX_REVE !== void 0 ? _process$env$MIX_REVE : 'https') === 'https',
